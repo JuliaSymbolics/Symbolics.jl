8f13af3c6fa9fd7e5036c7e2d542fd1cc4bdb6bf
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-02-26 08:30:26 -0500

    fix typo

--- docs/make.jl
+++ docs/make.jl
@@ -6,19 +6,19 @@ makedocs(
     modules=[Symbolics],
     clean=true,doctest=false,
     format = Documenter.HTML(#analytics = "UA-90474609-3",
-                             assets = ["assets/favicon.ico"])
+                             assets = ["assets/favicon.ico"]),
                              #canonical="https://mtk.sciml.ai/stable/"),
     pages=[
         "Home" => "index.md",
         "Tutorials" => Any[
             "tutorials/symbolic_functions.md",
             "tutorials/auto_parallel.md",
-            "tutorials/converting_to_C.md",
+            "tutorials/converting_to_C.md"
         ],
         "Comparison Against SymPy" => "comparison.md",
         "highlevel.md",
         "build_function.md",
-        "IR.md",
+        "IR.md"
     ]
 )
 
