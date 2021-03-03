73e37e6c824b2b9b6a6a7fe61ffd6986e3d2d043
Joe Carpinelli <jdcarp@umd.edu>
2021-03-03 15:21:31 -0500

    Updating whitespace in tests, moving build_targets.jl from ModelingToolkit to Symbolics

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using SafeTestsets, Test
 
 @safetestset "Differentiation Test" begin include("diff.jl") end
 @safetestset "Overloading Test" begin include("overloads.jl") end
-@safetestset "Build Function Array Test" begin include("build_function_arrayofarray.jl") end
 @safetestset "Build Function Test" begin include("build_function.jl") end
+@safetestset "Build Function Array Test" begin include("build_function_arrayofarray.jl") end
+@safetestset "Build Targets Test" begin include("build_targets.jl") end
