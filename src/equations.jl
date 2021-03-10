d2af942fd753d116f78b2f6cdb4ab5f3f34f512d
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2021-03-10 13:35:29 +0530

    Update docs to reflect changes proposed in #103
    In response to request in #103, it was suggested that we update the docs to show Broadcasted usage instead of introducing a change that may need to be broken in the future.
    
    This PR adds 2 examples to that effect.

--- src/equations.jl
+++ src/equations.jl
@@ -35,11 +35,25 @@ julia> using Symbolics
 
 julia> @variables x y;
 
+julia> @variables A[1:3, 1:3] B[1:3, 1:3];
+
 julia> x ~ y
 x ~ y
 
 julia> x - y ~ 0
 x - y ~ 0
+
+julia> A .~ B
+3×3 Array{Equation,2}:
+ A₁ˏ₁ ~ B₁ˏ₁  A₁ˏ₂ ~ B₁ˏ₂  A₁ˏ₃ ~ B₁ˏ₃
+ A₂ˏ₁ ~ B₂ˏ₁  A₂ˏ₂ ~ B₂ˏ₂  A₂ˏ₃ ~ B₂ˏ₃
+ A₃ˏ₁ ~ B₃ˏ₁  A₃ˏ₂ ~ B₃ˏ₂  A₃ˏ₃ ~ B₃ˏ₃
+
+julia> A .~ 3x
+3×3 Array{Equation,2}:
+ A₁ˏ₁ ~ 3x  A₁ˏ₂ ~ 3x  A₁ˏ₃ ~ 3x
+ A₂ˏ₁ ~ 3x  A₂ˏ₂ ~ 3x  A₂ˏ₃ ~ 3x
+ A₃ˏ₁ ~ 3x  A₃ˏ₂ ~ 3x  A₃ˏ₃ ~ 3x
 ```
 """
 Base.:~(lhs::Num, rhs::Num) = Equation(value(lhs), value(rhs))
