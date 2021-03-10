d6dd93386366f29414670600c9368776bec2256c
Philip Bittihn <philip@bittihn.de>
2021-03-10 14:41:49 +0100

    Modified/Added tests for improved CTarget build_function

--- test/build_targets.jl
+++ test/build_targets.jl
@@ -16,6 +16,7 @@ expr = [a*x - x*y,-3y + x*y]
                                      lhsname=:internal_var___du,
                                      rhsnames=[:internal_var___u,:internal_var___p,:t]) ==
   """
+  #include <math.h>
   void diffeqf(double* internal_var___du, double* internal_var___u, double* internal_var___p, double t) {
     internal_var___du[0] = internal_var___p[0] * internal_var___u[0] + -1 * internal_var___u[0] * internal_var___u[1];
     internal_var___du[1] = internal_var___u[0] * internal_var___u[1] + -3 * internal_var___u[1];
@@ -65,18 +66,29 @@ let
     cfunc      = build_function(expression, variables; target = Symbolics.CTarget(), expression = Val{true})
 
     # Generated function should be out[0] = in[0], out[1] = in[1], out[2] = in[2], etc.
-    @test cfunc == "void diffeqf(double* du, double* RHS1) {\n  du[0] = RHS1[0];\n  du[1] = RHS1[1];\n  du[2] = RHS1[2];\n  du[3] = RHS1[3];\n  du[4] = RHS1[4];\n  du[5] = RHS1[5];\n  du[6] = RHS1[6];\n  du[7] = RHS1[7];\n  du[8] = RHS1[8];\n  du[9] = RHS1[9];\n  du[10] = RHS1[10];\n  du[11] = RHS1[11];\n}\n"
+    @test cfunc == "#include <math.h>\nvoid diffeqf(double* du, double* RHS1) {\n  du[0] = RHS1[0];\n  du[1] = RHS1[1];\n  du[2] = RHS1[2];\n  du[3] = RHS1[3];\n  du[4] = RHS1[4];\n  du[5] = RHS1[5];\n  du[6] = RHS1[6];\n  du[7] = RHS1[7];\n  du[8] = RHS1[8];\n  du[9] = RHS1[9];\n  du[10] = RHS1[10];\n  du[11] = RHS1[11];\n}\n"
 end
 
 # Scalar CTarget test
 let
-    @variables x y z
-    expression = x + y + z
-    cfunc = build_function(expression, [x], [y], [z]; target = Symbolics.CTarget(), expression = Val{true})
+    @variables x y t
+    expression = x + y + t
+    cfunc = build_function(expression, [x], [y], t; target = Symbolics.CTarget(), expression = Val{true})
 
-    # Generated function should be out[0] = in1[0] + in2[0] + in3[0]
-    @test cfunc == "void diffeqf(double* du, double* RHS1, double* RHS2, double* RHS3) {\n  du[0] = RHS1[0] + RHS2[0] + RHS3[0];\n}\n"
+    # Generated function should be out[0] = in1[0] + in2[0] + in3
+    @test cfunc == "#include <math.h>\nvoid diffeqf(double* du, double* RHS1, double* RHS2, double RHS3) {\n  du[0] = RHS3 + RHS1[0] + RHS2[0];\n}\n"
 end
+
+# Scalar CTarget test with scalar multiplication and powers
+let
+  @variables x y a t
+  expression = x^2 + y^-1 + sin(a)^3.5 + 2t
+  cfunc = build_function(expression, [x, y], [a], t; target = Symbolics.CTarget(), expression = Val{true})
+
+  # Generated function should avoid scalar multiplication of the form "4t" (currently done by adding another "* 1") and other invalid C syntax
+  @test cfunc == "#include <math.h>\nvoid diffeqf(double* du, double* RHS1, double* RHS2, double RHS3) {\n  du[0] = 2 * RHS3 * 1 + pow(RHS1[0], 2) + 1.0 / RHS1[1] + pow(sin(RHS2[0]), 3.5);\n}\n"
+end
+
 
 # Matrix StanTarget test
 let
