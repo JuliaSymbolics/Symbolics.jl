11a6c52af19393a6bf3ec5013f1cc0110dd9fdc5
Yingbo Ma <mayingbo5@gmail.com>
2021-03-23 17:57:25 -0400

    Fix broken test

--- test/build_function.jl
+++ test/build_function.jl
@@ -139,4 +139,4 @@ using SymbolicUtils.Code: Func, toexpr
 D = Differential(t)
 expr = toexpr(Func([D(x)], [], D(x)))
 @test expr.args[2].args[end] == expr.args[1].args[1] # check function body and function arg
-@test expr.args[2].args[end] == :(xˍt(t))
+@test expr.args[2].args[end] == :(var"(Differential(t))(x)"(t))
