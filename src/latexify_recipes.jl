302f03bcae7498fe10bded0980075e10ad65a3ee
Luke Adams <luke@lukeclydeadams.com>
2021-04-06 16:28:05 -0600

    Remove canonicalize argument in _toexpr
    As far as I can tell, the only purpose of this was to make the
    transformations like
    x^-2 -> inv(x)^2.
    However, this behavior was not being used, and it does not seem
    desirable.

--- src/latexify_recipes.jl
+++ src/latexify_recipes.jl
@@ -11,13 +11,13 @@ prettify_expr(expr::Expr) = Expr(expr.head, prettify_expr.(expr.args)...)
     # that latexify can deal with
 
     rhs = getfield.(eqs, :rhs)
-    rhs = prettify_expr.(_toexpr(rhs; canonicalize=false))
+    rhs = prettify_expr.(_toexpr(rhs))
     rhs = [postwalk(x -> x isa Expr && length(x.args) == 1 ? x.args[1] : x, eq) for eq in rhs]
     rhs = [postwalk(x -> x isa Expr && x.args[1] == :_derivative && length(x.args[2].args) == 2 ? :($(Symbol(:d, x.args[2]))/($(Symbol(:d, x.args[2].args[2])))) : x, eq) for eq in rhs]
     rhs = [postwalk(x -> x isa Expr && x.args[1] == :_derivative ? "\\frac{d\\left($(Latexify.latexraw(x.args[2]))\\right)}{d$(Latexify.latexraw(x.args[3]))}" : x, eq) for eq in rhs]
 
     lhs = getfield.(eqs, :lhs)
-    lhs = prettify_expr.(_toexpr(lhs; canonicalize=false))
+    lhs = prettify_expr.(_toexpr(lhs))
     lhs = [postwalk(x -> x isa Expr && length(x.args) == 1 ? x.args[1] : x, eq) for eq in lhs]
     lhs = [postwalk(x -> x isa Expr && x.args[1] == :_derivative && length(x.args[2].args) == 2 ? :($(Symbol(:d, x.args[2]))/($(Symbol(:d, x.args[2].args[2])))) : x, eq) for eq in lhs]
     lhs = [postwalk(x -> x isa Expr && x.args[1] == :_derivative ? "\\frac{d\\left($(Latexify.latexraw(x.args[2]))\\right)}{d$(Latexify.latexraw(x.args[3]))}" : x, eq) for eq in lhs]
@@ -31,60 +31,38 @@ Base.show(io::IO, ::MIME"text/latex", x::Vector{Equation}) = print(io, latexify(
 Base.show(io::IO, ::MIME"text/latex", x::AbstractArray{Num}) = print(io, latexify(x))
 
 @latexrecipe function f(n::Num)
-    return _toexpr(n, canonicalize=false)
+    return _toexpr(n)
 end
 
 # `_toexpr` is only used for latexify
-function _toexpr(O; canonicalize=true)
-    if canonicalize
-        canonical, O = canonicalexpr(O)
-        canonical && return O
-    else
-        !istree(O) && return O
-    end
+function _toexpr(O)
+    !istree(O) && return O
+
     op = operation(O)
     args = arguments(O)
 
     if (op===(*)) && (args[1] === -1)
-        arg_mul = Expr(:call, :(*), _toexpr(args[2:end]; canonicalize=canonicalize)...)
+        arg_mul = Expr(:call, :(*), _toexpr(args[2:end])...)
         return Expr(:call, :(-), arg_mul)
     end
 
     if op isa Differential
-        ex = _toexpr(args[1]; canonicalize=canonicalize)
-        wrt = _toexpr(op.x; canonicalize=canonicalize)
+        ex = _toexpr(args[1])
+        wrt = _toexpr(op.x)
         return :(_derivative($ex, $wrt))
     elseif op isa Sym
         isempty(args) && return nameof(op)
-        return Expr(:call, _toexpr(op; canonicalize=canonicalize), _toexpr(args; canonicalize=canonicalize)...)
+        return Expr(:call, _toexpr(op), _toexpr(args)...)
     end
-    return Expr(:call, Symbol(op), _toexpr(args; canonicalize=canonicalize)...)
+    return Expr(:call, Symbol(op), _toexpr(args)...)
 end
-_toexpr(s::Sym; kw...) = nameof(s)
-_toexpr(x::Integer; kw...) = x
-_toexpr(x::AbstractFloat; kw...) = x
+_toexpr(s::Sym) = nameof(s)
+_toexpr(x::Integer) = x
+_toexpr(x::AbstractFloat) = x
 
-function _toexpr(eq::Equation; kw...)
-    Expr(:(=), _toexpr(eq.lhs; kw...), _toexpr(eq.rhs; kw...))
+function _toexpr(eq::Equation)
+    Expr(:(=), _toexpr(eq.lhs), _toexpr(eq.rhs))
 end
 
-_toexpr(eqs::AbstractArray; kw...) = map(eq->_toexpr(eq; kw...), eqs)
-_toexpr(x::Num; kw...) = _toexpr(value(x); kw...)
-
-function canonicalexpr(O)
-    !istree(O) && return true, O
-    op = operation(O)
-    args = arguments(O)
-    if op === (^)
-        if length(args) == 2 && args[2] isa Number && args[2] < 0
-            ex = _toexpr(args[1])
-            if args[2] == -1
-                expr = Expr(:call, :inv, ex)
-            else
-                expr = Expr(:call, :^, Expr(:call, inv, ex), -args[2])
-            end
-            return true, expr
-        end
-    end
-    return false, O
-end
+_toexpr(eqs::AbstractArray) = map(eq->_toexpr(eq), eqs)
+_toexpr(x::Num) = _toexpr(value(x))
