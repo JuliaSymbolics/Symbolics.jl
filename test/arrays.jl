433ed0c2e448060ba20ec7ddba65e925948d995b
Shashi Gowda <gowda@mit.edu>
2021-04-09 05:20:43 +0530

    add to runtests

--- test/arrays.jl
+++ test/arrays.jl
@@ -1,23 +1,23 @@
 using SymbolicUtils, Test
-using SymbolicUtils: getmetadata, symtype, ArrayShape, ArrayShapeCtx
+using SymbolicUtils: symtype, shape, Unknown
 using Base: Slice
 
 @testset "arrays" begin
     @syms X[1:5, 2:6] (Y::Real)[1:5, 1:5] Z::Matrix{Float64} i::Int j::Int
     @test symtype(X) == Array{Number, 2}
     @test symtype(Y) == Array{Real, 2}
-    @test getmetadata(X, ArrayShapeCtx) == ArrayShape(Slice.((1:5, 2:6)))
-    @test getmetadata(Y, ArrayShapeCtx) == ArrayShape(Slice.((1:5, 1:5)))
+    @test shape(X) == Slice.((1:5, 2:6))
+    @test shape(Y) == Slice.((1:5, 1:5))
 
-    A = Y[2, :]
-    @test symtype(A) == Array{Real, 1}
-    @test axes(getmetadata(A, ArrayShapeCtx)) == (1:5,)
+    A = Y[2, :] # Maybe it should be Array vv
+    @test symtype(A) == AbstractArray{Real, 1}
+    @test axes(A) == (1:5,)
 
     B = A[3:5]
-    @test axes(getmetadata(B, ArrayShapeCtx)) == (Slice(1:3),)
+    @test axes(B) == (Slice(1:3),)
 
-    @test_throws ArgumentError getmetadata(Z[1:2, 3:4], ArrayShapeCtx)
+    @test shape(Z[1:2, 3:4]) isa Unknown
 
-    X[i,j]
-    X[1,j]
+    @test symtype(X[i,j]) == Number
+    @test symtype(X[1,j]) == Number
 end
--- test/fuzz-arrays.jl
+++ test/fuzz-arrays.jl
@@ -2,6 +2,8 @@ using SymbolicUtils
 using Test
 using LinearAlgebra
 
+# Transpose and multiply
+
 @syms A[1:2, 1:3] B[1:3, 1:1] C::Matrix{Number} x[1:2] y[1:3] z::Vector{Number}
 
 AA = rand(2,3); BB = rand(3,2); xx = rand(2); yy = rand(3)
@@ -60,4 +62,18 @@ function rand_mul_expr(a=rand_leaf(), b=rand_leaf())
             @test size(a * b) == size(rand(size(a)...) * rand(size(b)...))
         end
     end
+end
+
+
+@testset "fuzz ' and *" begin
+    for i=1:1000
+        rand_mul_expr()
+    end
+end
+
+## Getindex fuzz
+#
+
+function rand_getindex_expr(x=rand_leaf())
+    n = ndims(x)
 end
