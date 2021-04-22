7b232fb01b95a0e39cbbb4f675750d3ef2ba9402
Yingbo Ma <mayingbo5@gmail.com>
2021-04-22 12:43:57 -0400

    Oops

--- test/macro.jl
+++ test/macro.jl
@@ -1,7 +1,7 @@
-import ModelingToolkit
+import Symbolics
 using Test
 
 @test !(@isdefined Num)
-ModelingToolkit.@variables t
-ModelingToolkit.@register fff(t)
-@test isequal(fff(t), ModelingToolkit.Num(ModelingToolkit.Term{Real}(fff, [ModelingToolkit.value(t)])))
+Symbolics.@variables t
+Symbolics.@register fff(t)
+@test isequal(fff(t), Symbolics.Num(Symbolics.Term{Real}(fff, [Symbolics.value(t)])))
