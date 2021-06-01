4871571b818ef6369d42ba59d2639c050572036b
Yingbo Ma <mayingbo5@gmail.com>
2021-06-01 12:49:04 -0400

    See if this works

--- test/sympy.jl
+++ test/sympy.jl
@@ -1,3 +1,17 @@
+if ENV["CI"]
+    # copied and modified from https://github.com/tkf/IPython.jl/blob/master/test/install_dependencies.jl
+
+
+    # Adding Pkg in test/REQUIRE would be an error in 0.6.  Using
+    # Project.toml still has some gotchas.  So:
+    Pkg = Base.require(Base.PkgId(Base.UUID(0x44cfe95a1eb252eab672e2afdf69b78f), "Pkg"))
+
+    # Let PyCall.jl use Python interpreter from Conda.jl
+    # See: https://github.com/JuliaPy/PyCall.jl
+    ENV["PYTHON"] = ""
+    Pkg.build("PyCall")
+end
+
 using Test
 using SymPy
 using Symbolics
