b0d7662b65797b5c6bf71b7b33a2e247f7cf2d2e
Shashi Gowda <gowda@mit.edu>
2021-06-11 09:56:06 +0530

    test wrapper types

--- test/macro.jl
+++ test/macro.jl
@@ -1,5 +1,5 @@
 import Symbolics
-import Symbolics: getdefaultval
+import Symbolics: getdefaultval, wrap, unwrap
 import SymbolicUtils: Term, symtype, FnType
 using Test
 
@@ -7,6 +7,8 @@ using Test
 @variables t
 Symbolics.@register fff(t)
 @test isequal(fff(t), Symbolics.Num(Symbolics.Term{Real}(fff, [Symbolics.value(t)])))
+
+## @variables
 
 @variables t=0 a=1 x[1:4]=2 y[1:4](t)=3 w[1:4] = 1:4 z[1:4](t) = 2:5 p[1:4](..)
 
@@ -22,3 +24,39 @@ Symbolics.@register fff(t)
 @test p[1] isa Term
 @test symtype(p[1]) <: FnType{Tuple, Real}
 @test p[1](t) isa Num
+
+
+## Wrapper types
+
+abstract type AbstractFoo{T} end
+
+struct Foo{T}<:AbstractFoo{T}
+end
+@symbolic_wrap struct FooWrap{T} <: AbstractFoo{T}
+    val::Foo{T}
+end
+
+Symbolics.unwrap(r::FooWrap) = r.val
+
+@test Symbolics.wrapper_type(AbstractFoo) == FooWrap
+@test Symbolics.wrapper_type(AbstractFoo{Int}) == FooWrap # removes the type param.
+                                                          # If required,
+                                                          # a new method must be defined
+                                                          # by the user of @symbolic_wrap
+
+x = Foo{Int}()
+@test wrap(x) isa FooWrap{Int}
+@test unwrap(wrap(x)) isa Foo{Int}
+
+@wrapped function foo(f::AbstractFoo, x::Real)
+    x > 1 ? (x > 5 ? "hi" : Foo{Int}()) : 0
+end
+
+@test !applicable(foo, x, 2)
+@test applicable(foo, x, wrap(2))
+@test applicable(foo, wrap(x), 2)
+@test applicable(foo, wrap(x), wrap(2))
+
+@test foo(x, wrap(2)) isa FooWrap
+@test foo(x, wrap(1)) isa Num
+@test foo(x, wrap(6)) isa String
