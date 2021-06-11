f4f999826df2992b5c43865f29790f890ca1ee7a
Shashi Gowda <gowda@mit.edu>
2021-06-11 09:36:33 +0530

    cover one more case

--- test/macro.jl
+++ test/macro.jl
@@ -1,5 +1,6 @@
 import Symbolics
 import Symbolics: getdefaultval
+import SymbolicUtils: Term, symtype, FnType
 using Test
 
 @test !(@isdefined Num)
@@ -7,7 +8,7 @@ using Test
 Symbolics.@register fff(t)
 @test isequal(fff(t), Symbolics.Num(Symbolics.Term{Real}(fff, [Symbolics.value(t)])))
 
-@variables t=0 a=1 x[1:4]=2 y[1:4](t)=3 w[1:4] = 1:4 z[1:4](t) = 2:5
+@variables t=0 a=1 x[1:4]=2 y[1:4](t)=3 w[1:4] = 1:4 z[1:4](t) = 2:5 p[1:4](..)
 
 @test getdefaultval(t) == 0
 @test getdefaultval(a) == 1
@@ -17,3 +18,7 @@ Symbolics.@register fff(t)
 @test getdefaultval(w[2]) == 2
 @test getdefaultval(w[4]) == 4
 @test getdefaultval(z[3]) == 4
+
+@test p[1] isa Term
+@test symtype(p[1]) <: FnType{Tuple, Real}
+@test p[1](t) isa Num
