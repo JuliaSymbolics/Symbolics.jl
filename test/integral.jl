530f3d54314dc740f617bcbe6689978d45af92de
ashutosh-b-b <a.bharambe123@gmail.com>
2021-06-13 02:01:48 +0530

    Add tests

--- test/integral.jl
+++ test/integral.jl
@@ -5,3 +5,14 @@ using DomainSets
 I1 = Integral( x , ClosedInterval(1, 5))
 I2 = Integral( x , ClosedInterval(1, 5))
 @test I1 == I2
+
+@variables  v(..) , u(..) , x , y
+D = Differential(x)
+Dxx = Differential(x)^2
+I = Integral(x, ClosedInterval(1 , v(x)))
+eq = D((I(u(x,t)) )) ~ 0
+O = expand_derivatives(eq.lhs)
+println(O)
+eq = Dxx((I(u(x,t)) )) ~ 0
+O = expand_derivatives(eq.lhs)
+println(O)
