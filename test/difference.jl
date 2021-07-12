db66dad47436e1f424daaf763c63be7e1faaaf55
Sharan Yalburgi <sharanyalburgi@gmail.com>
2021-07-12 14:11:11 +0530

    Add more tests

--- test/difference.jl
+++ test/difference.jl
@@ -1,10 +1,13 @@
 using Symbolics
 using Test
 
-@variables t
+@variables t, x
 D1 = Difference(t; dt=0.01)
 D2 = Difference(t; dt=0.01)
 
 @test D1 == D2
 @test Base.isequal(D1, D2)
 @test Base.hash(D1) == Base.hash(D2)
+
+@test D1(x) isa Num
+@test repr(D1) == "Difference(t; dt=0.01)"
