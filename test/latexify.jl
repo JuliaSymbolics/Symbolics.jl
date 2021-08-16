0c6264a475a7970a1f0c1945ada555016c8ece77
Jan Kosata <58295890+jkosata@users.noreply.github.com>
2021-08-16 21:10:38 +0200

    fix latexifying derivatives (#344)
    Co-authored-by: hrochan <kosataj@phys.ethz.ch>

--- test/latexify.jl
+++ test/latexify.jl
@@ -5,6 +5,7 @@ using ReferenceTests
 
 @variables x y z u(x)
 Dx = Differential(x)
+Dy = Differential(y)
 
 # issue 260
 @test Symbolics._toexpr(3*x/y) == :((3x) / y)
@@ -25,6 +26,7 @@ Dx = Differential(x)
 @test_reference "latexify_refs/derivative1.txt" latexify(Dx(y))
 @test_reference "latexify_refs/derivative2.txt" latexify(Dx(u))
 @test_reference "latexify_refs/derivative3.txt" latexify(Dx(x^2 + y^2 + z^2))
+@test_reference "latexify_refs/derivative4.txt" latexify(Dy(u))
 
 @test_reference "latexify_refs/stable_mul_ordering1.txt" latexify(x * y)
 @test_reference "latexify_refs/stable_mul_ordering2.txt" latexify(y * x)
