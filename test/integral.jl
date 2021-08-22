4bda938a1dc8a184c7af11201f1af7052d4f8bd3
ashutosh-b-b <a.bharambe123@gmail.com>
2021-08-23 01:47:22 +0530

    Remove x from Integral and use VarDomainParing

--- test/integral.jl
+++ test/integral.jl
@@ -2,14 +2,14 @@ using Symbolics, Test
 using DomainSets
 @variables x y
 
-I1 = Integral(x, ClosedInterval(1, 5))
-I2 = Integral(x, ClosedInterval(1, 5))
+I1 = Integral(x in ClosedInterval(1, 5))
+I2 = Integral(x in ClosedInterval(1, 5))
 @test I1 == I2
 
 @variables v(..) u(..) x y
 D = Differential(x)
 Dxx = Differential(x)^2
-I = Integral(y, ClosedInterval(1, 5))
+I = Integral(y in ClosedInterval(1, 5))
 eq = D(I(u(x,y))) ~ 0
 eq_test = I(D(u(x,y)))
 @test isequal(expand_derivatives(eq.lhs), Symbolics.value(eq_test))
@@ -18,12 +18,12 @@ eq = Dxx((I(u(x,y)))) + I(D(u(x,y))) ~ 0
 eq_test = I(D(u(x,y))) + I(D(D(u(x,y))))
 @test isequal(expand_derivatives(eq.lhs), Symbolics.value(eq_test))
 
-I = Integral(y, ClosedInterval(1, v(x)))
+I = Integral(y in ClosedInterval(1, v(x)))
 eq = D((I(u(x,y)))) ~ 0
 eq_test = I(D(u(x,y))) + D(v(x))*u(x, v(x))
 @test isequal(expand_derivatives(eq.lhs), Symbolics.value(eq_test))
 
-I = Integral(y, ClosedInterval(1, v(x)))
+I = Integral(y in ClosedInterval(1, v(x)))
 eq = Dxx((I(u(x,y)))) ~ 0
 eq_test = D(I(D(u(x,y))) + D(v(x))*u(x, v(x)))
 eq_test_ = I(D(D(u(x,y)))) + D(D(v(x)))*u(x, v(x)) + 2D(u(x,v(x)))*D(v(x))
