ef4bcbc36f0ed1591b75ff5c0e2d8522f69d8486
Shashi Gowda <gowda@mit.edu>
2021-09-01 18:32:00 -0400

    check in benchmark script

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,7 +1,7 @@
 # A little trick for travis
-using PkgBenchmark, SymbolicUtils
+using PkgBenchmark, Symbolics
 
-pkgpath = dirname(dirname(pathof(SymbolicUtils)))
+pkgpath = dirname(dirname(pathof(Symbolics)))
 # move it out of the repository so that you can check out different branches
 script = tempname() * ".jl"
 benchpath = joinpath(pkgpath, "benchmark", "benchmarks.jl")
