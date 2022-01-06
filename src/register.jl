bfc9a7a13f50cfdb5310f6a6b16fdc6d4ed2e261
Shashi Gowda <gowda@mit.edu>
2022-01-06 05:13:40 -0500

    MethodError in case of non-symbolic values, return Terms when args are unwrapped

--- src/register.jl
+++ src/register.jl
@@ -51,17 +51,22 @@ macro register_symbolic(expr, define_promotion = true, Ts = [])
     eval_method = :(@eval function $f($(Expr(:$, :(s...))),)
                         args = [$(Expr(:$, :(s_syms...)))]
                         unwrapped_args = map($unwrap, args)
-                        if !any(x->$issym(x) || $istree(x), unwrapped_args)
+                        res = if !any(x->$issym(x) || $istree(x), unwrapped_args)
                             if typeof.(args) == typeof.(unwrapped_args)
                                 # this means there was no method which already caught it.
-                                # so create a term
-                                $Term{$ret_type}($f, unwrapped_args)
+                                # so rethrow method error
+                                throw(MethodError($f, unwrapped_args))
                             else
                                 $f(unwrapped_args...)
                             end
                         else
                             $Term{$ret_type}($f, unwrapped_args)
-                        end |> $wrap
+                        end
+                        if typeof.(args) == typeof.(unwrapped_args)
+                            return res
+                        else
+                            return $wrap(res)
+                        end
                     end)
     verbose = false
     mod, fname = f isa Expr && f.head == :(.) ? f.args : (:(@__MODULE__), QuoteNode(f))
