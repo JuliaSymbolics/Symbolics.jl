821463fd9bd9704ba5a757b82605a80d5b9008b4
Shashi Gowda <gowda@mit.edu>
2022-03-09 23:17:49 -0500

    update matlab tests?

--- test/target_functions/matrix.m
+++ test/target_functions/matrix.m
@@ -1,4 +1,4 @@
-diffeqf = @(t,internal_var___u) [
+diffeqf = @(internal_var___t,internal_var___u) [
   internal_var___u(1), internal_var___u(5), internal_var___u(9);
   internal_var___u(2), internal_var___u(6), internal_var___u(10);
   internal_var___u(3), internal_var___u(7), internal_var___u(11);
--- test/target_functions/scalar1.m
+++ test/target_functions/scalar1.m
@@ -1,3 +1,3 @@
-diffeqf = @(t,internal_var___u) [
+diffeqf = @(internal_var___t,internal_var___u) [
   internal_var___u(1) + internal_var___u(2) + internal_var___u(3);
 ];
