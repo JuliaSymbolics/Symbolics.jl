a0e8443be6af2b8cb05bc9a454bde25f9a8e5132
xtalax <alex.lemony@gmail.com>
2022-04-08 13:50:33 +0100

    2D diffusion Stencils

--- test/arrays.jl
+++ test/arrays.jl
@@ -12,7 +12,7 @@ using SymbolicUtils: Sym, term, operation
     @test shape(Y) == Slice.((1:5, 1:5))
 
     A = Y[2, :]
-    @test typeof(A) <: Arr{Num, 1}
+    @test typeof(A) <: Arr{Num,1}
     @test axes(A) == (1:5,)
 
     B = A[3:5]
@@ -20,8 +20,8 @@ using SymbolicUtils: Sym, term, operation
 
     i = Sym{Int}(:i)
     j = Sym{Int}(:j)
-    @test symtype(X[i,j]) == Real
-    @test symtype(X[1,j]) == Real
+    @test symtype(X[i, j]) == Real
+    @test symtype(X[1, j]) == Real
 
     @variables t x[1:2](t)
     @test isequal(get_variables(0 ~ x[1]), [x[1]])
@@ -32,12 +32,12 @@ end
 @testset "getindex" begin
     @variables X[1:5, 1:5] Y[1:5, 1:5]
 
-    @test isequal(X[1,1], wrap(term(getindex, unwrap(X), 1,1)))
+    @test isequal(X[1, 1], wrap(term(getindex, unwrap(X), 1, 1)))
 
     XX = unwrap(X)
-    @test isequal(unwrap(X[1, :]), Symbolics.@arrayop(XX[1,:], (j,), XX[1, j]))
-    @test isequal(unwrap(X[:, 2]), Symbolics.@arrayop(XX[:,2], (i,), XX[i, 2]))
-    @test isequal(unwrap(X[:, 2:3]), Symbolics.@arrayop(XX[:,2:3], (i,j), XX[i, j], (+), (j in 2:3)))
+    @test isequal(unwrap(X[1, :]), Symbolics.@arrayop(XX[1, :], (j,), XX[1, j]))
+    @test isequal(unwrap(X[:, 2]), Symbolics.@arrayop(XX[:, 2], (i,), XX[i, 2]))
+    @test isequal(unwrap(X[:, 2:3]), Symbolics.@arrayop(XX[:, 2:3], (i, j), XX[i, j], (+), (j in 2:3)))
 
     @variables t x[1:4](t)
     @syms i::Int
@@ -46,32 +46,32 @@ end
 
 getdef(v) = getmetadata(v, Symbolics.VariableDefaultValue)
 @testset "broadcast & scalarize" begin
-    @variables A[1:5,1:3]=42 b[1:3]=[2, 3, 5] t x[1:4](t) u[1:1]
+    @variables A[1:5, 1:3] = 42 b[1:3] = [2, 3, 5] t x[1:4](t) u[1:1]
     AA = Symbolics.scalarize(A)
     bb = Symbolics.scalarize(b)
     @test all(isequal(42), getdef.(AA))
     @test getdef.(bb) == [2, 3, 5]
-    @test isequal(Symbolics.scalarize([b.*1; b.*1]), [bb; bb])
-    @test isequal(Symbolics.scalarize(b.^1), bb)
-    c = A*b
+    @test isequal(Symbolics.scalarize([b .* 1; b .* 1]), [bb; bb])
+    @test isequal(Symbolics.scalarize(b .^ 1), bb)
+    c = A * b
 
     @test isequal(collect(sin.(x)),
-                  sin.([x[i] for i in 1:4]))
+        sin.([x[i] for i in 1:4]))
 
-    @test isequal(Symbolics.scalarize(sin.(A.*c)[1,1]),
-                  sin(A[1, 1]*(b[1]*A[1, 1] +
-                               b[2]*A[1, 2] +
-                               b[3]*A[1, 3])))
+    @test isequal(Symbolics.scalarize(sin.(A .* c)[1, 1]),
+        sin(A[1, 1] * (b[1] * A[1, 1] +
+                       b[2] * A[1, 2] +
+                       b[3] * A[1, 3])))
 
     D = Differential(t)
-    @test isequal(collect(D.(x) ~ x), map(i->D(x[i]) ~ x[i], eachindex(x)))
+    @test isequal(collect(D.(x) ~ x), map(i -> D(x[i]) ~ x[i], eachindex(x)))
     @test_throws ArgumentError A ~ t
 
     # #448
     @test isequal(Symbolics.scalarize(u + u), [2u[1]])
 
     # #417
-    @test isequal(Symbolics.scalarize(x', (1,1)), x[1])
+    @test isequal(Symbolics.scalarize(x', (1, 1)), x[1])
 end
 
 @testset "Parent" begin
@@ -81,8 +81,8 @@ end
 end
 
 n = 2
-A = randn(n,n)
-foo(x) = A*x # a function to represent symbolically, note, if this function is defined inside the testset, it's not found by the function fun_eval = eval(fun_ex)
+A = randn(n, n)
+foo(x) = A * x # a function to represent symbolically, note, if this function is defined inside the testset, it's not found by the function fun_eval = eval(fun_ex)
 function Symbolics.propagate_ndims(::typeof(foo), x)
     ndims(x)
 end
@@ -102,31 +102,31 @@ The following two testsets test jacobians for symbolic functions of symbolic arr
     @variables x[1:n]
 
     function symbolic_call(x)
-        @syms foo(x::Symbolics.Arr{Num, 1})::Symbolics.Arr{Num, 1} # symbolic foo can not be created in global scope due to conflict with function foo
+        @syms foo(x::Symbolics.Arr{Num,1})::Symbolics.Arr{Num,1} # symbolic foo can not be created in global scope due to conflict with function foo
         foo(x) # return a symbolic call to foo
     end
 
     x0 = randn(n)
-    @test foo(x0) == A*x0
+    @test foo(x0) == A * x0
     ex = symbolic_call(x)
 
     fun_genf = build_function(ex, x, expression=Val{false})
-    @test_broken fun_genf(x0) == A*x0# UndefVarError: foo not defined
+    @test_broken fun_genf(x0) == A * x0# UndefVarError: foo not defined
 
     # Generate an expression instead and eval it manually
     fun_ex = build_function(ex, x, expression=Val{true})
     fun_eval = eval(fun_ex)
-    @test fun_eval(x0) == foo(x0) 
+    @test fun_eval(x0) == foo(x0)
 
     # Try to provide the hidden argument `expression_module` to solve the scoping issue
     @test_skip begin
         fun_genf = build_function(ex, x, expression=Val{false}, expression_module=Main) # UndefVarError: #_RGF_ModTag not defined
-        fun_genf(x0) == A*x0 
+        fun_genf(x0) == A * x0
     end
 
     ## Jacobians
     @test Symbolics.value.(Symbolics.jacobian(foo(x), x)) == A
-    @test_throws ErrorException Symbolics.value.(Symbolics.jacobian(ex , x))
+    @test_throws ErrorException Symbolics.value.(Symbolics.jacobian(ex, x))
 end
 
 
@@ -134,26 +134,75 @@ end
     @variables x[1:n]
 
     x0 = randn(n)
-    @test foo(x0) == A*x0
+    @test foo(x0) == A * x0
     ex = foo(x)
 
     @test shape(ex) == shape(x)
 
     fun_genf = build_function(ex, x, expression=Val{false})
-    @test fun_genf(x0) == A*x0
+    @test fun_genf(x0) == A * x0
 
     # Generate an expression instead and eval it manually
     fun_ex = build_function(ex, x, expression=Val{true})
     fun_eval = eval(fun_ex)
-    @test fun_eval(x0) == foo(x0) 
+    @test fun_eval(x0) == foo(x0)
 
     ## Jacobians
     @test value.(jacobian(foo(x), x)) == A
-    @test value.(jacobian(ex , x)) == A
+    @test value.(jacobian(ex, x)) == A
 end
 
 @testset "Rules" begin
-    @variables X[1:10,1:5] Y[1:5, 1:10] b[1:10];
-    r = @rule ((~A*~B)*~C) => (~A*(~B*~C)) where size(~A,1)*size(~B,2) > size(~B,1)*size(~C,2)
-    @test isequal(r(unwrap((X*Y)*b)), unwrap(X*(Y*b)))
+    @variables X[1:10, 1:5] Y[1:5, 1:10] b[1:10]
+    r = @rule ((~A * ~B) * ~C) => (~A * (~B * ~C)) where {{size(~A, 1)}*size(~B, 2)} > size(~B, 1) * size(~C, 2)
+    @test isequal(r(unwrap((X * Y) * b)), unwrap(X * (Y * b)))
+end
+
+@testset "2D Diffusion Composed With Stencil Interface" begin
+    n = rand(8:32)
+
+    @makearray u[1:n, 1:n] begin
+        #interior
+        u[2:end-1, 2:end-1] => @arrayop (i, j) u[i-1, j] + u[i+1, j] + u[i, j-1] + u[i, j+1] - 4 * u[i, j]
+        #BCs
+        u[1, 1:end-1] => @arrayop (i, j) 0.0
+        u[n, 1:end-1] => @arrayop (i, j) 0.0
+        u[1:end-1, 1] => @arrayop (i, j) 0.0
+        u[1:end-1, n] => @arrayop (i, j) 0.0
+        #corners
+        u[1, 1] => @arrayop (i, j) 0.0
+        u[n, 1] => @arrayop (i, j) 0.0
+        u[1, n] => @arrayop (i, j) 0.0
+        u[n, n] => @arrayop (i, j) 0.0
+    end
+
+    #2D Diffusion composed
+    @makearray ucx[1:n, 1:n] begin
+        uc[2:end-1, 2:end-1] => @arrayop (i, j) u[i-1, j] + u[i+1, j] - 2 * u[i, j]
+    end
+
+    @makearray ucy[2:end-1, 2:end-1] begin
+        ucy[2:end-1, 2:end-1] => @arrayop (i, j) u[i, j-1] + u[i, j+1] - 2 * u[i, j]
+    end
+
+    uc = ucx .+ ucy
+    # BCs
+    @setview! uc[1, 2:end-1] @arrayop (i, j) 0.0
+
+
+    @setview! uc[n, 2:end-1] @arrayop (i, j) 0.0
+
+
+    @setview! uc[2:end-1, 1] @arrayop (i, j) 0.0
+
+
+    @setview! uc[2:end-1, n] @arrayop (i, j) 0.0
+
+    # Corners
+    @setview! u[1, 1] @arrayop (i, j) 0.0
+    @setview! u[n, 1] @arrayop (i, j) 0.0
+    @setview! u[1, n] @arrayop (i, j) 0.0
+    @setview! u[n, n] @arrayop (i, j) 0.0
+
+    @test u == uc
 end
