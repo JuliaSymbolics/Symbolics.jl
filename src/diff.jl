d1db9e642faff8e354796c179abd1955766aa74e
Shashi Gowda <gowda@mit.edu>
2022-04-26 11:37:19 -0400

    salarize inputs to jacobian

--- src/diff.jl
+++ src/diff.jl
@@ -416,6 +416,7 @@ A helper function for computing the Jacobian of an array of expressions with res
 an array of variable expressions.
 """
 function jacobian(ops::AbstractVector, vars::AbstractVector; simplify=false)
+    ops = Symbolics.scalarize.(ops)
     Num[Num(expand_derivatives(Differential(value(v))(value(O)),simplify)) for O in ops, v in vars]
 end
 
