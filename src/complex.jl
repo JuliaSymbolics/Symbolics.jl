13a139358d7e2b9956d88d5fca3701c194551f86
davidschlegel <schlegel.david.ds@gmail.com>
2022-11-14 17:11:25 +0100

    Add function metadata for type ComplexTerm

--- src/complex.jl
+++ src/complex.jl
@@ -21,6 +21,7 @@ TermInterface.symtype(a::ComplexTerm{T}) where T = Complex{T}
 TermInterface.istree(a::ComplexTerm) = true
 TermInterface.operation(a::ComplexTerm{T}) where T = Complex{T}
 TermInterface.arguments(a::ComplexTerm) = [a.re, a.im]
+TermInterface.metadata(a::ComplexTerm) = a.re.metadata
 
 function TermInterface.similarterm(t::ComplexTerm, f, args, symtype; metadata=nothing, exprhead=exprhead(t))
     if f <: Complex
