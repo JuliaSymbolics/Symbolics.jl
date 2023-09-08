442677aa9526cde88b0d1451b3a50b61cac50886
Shashi Gowda <gowda@mit.edu>
2023-09-08 11:24:20 -0400

    remove unused function

--- src/register.jl
+++ src/register.jl
@@ -69,32 +69,3 @@ end
 
 Base.@deprecate_binding var"@register" var"@register_symbolic"
 
-# Ensure that Num that get @registered from outside the ModelingToolkit
-# module can work without having to bring in the associated function into the
-# ModelingToolkit namespace. We basically store information about functions
-# registered at runtime in a ModelingToolkit variable,
-# `registered_external_functions`. It's not pretty, but we are limited by the
-# way GeneralizedGenerated builds a function (adding "ModelingToolkit" to every
-# function call).
-# ---
-const registered_external_functions = Dict{Symbol,Module}()
-function inject_registered_module_functions(expr)
-    MacroTools.postwalk(expr) do x
-        # Find all function calls in the expression and extract the function
-        # name and calling module.
-        MacroTools.@capture(x, f_module_.f_name_(xs__))
-        if isnothing(f_module)
-            MacroTools.@capture(x, f_name_(xs__))
-        end
-
-        if !isnothing(f_name)
-            # Set the calling module to the module that registered it.
-            mod = get(registered_external_functions, f_name, f_module)
-            if !isnothing(mod) && mod != Base
-                x.args[1] = :($mod.$f_name)
-            end
-        end
-
-        return x
-    end
-end
