252b095f63bbee55bcd9ecb3aefc57817d58c269
Documenter.jl <documenter@juliadocs.github.io>
2023-10-18 09:05:54 +0000

    build based on 0116b7d

--- dev/function.jl
+++ dev/function.jl
@@ -1,9 +1,9 @@
 function (ˍ₋out, ˍ₋arg1)
     begin
         begin
-            #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:399 =# @inbounds begin
-                    ˍ₋out[1] = (+)((^)(ˍ₋arg1[1], 2), ˍ₋arg1[2])
-                    ˍ₋out[2] = (+)((^)(ˍ₋arg1[2], 2), ˍ₋arg1[1])
+            #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:422 =# @inbounds begin
+                    ˍ₋out[1] = (+)(ˍ₋arg1[2], (^)(ˍ₋arg1[1], 2))
+                    ˍ₋out[2] = (+)(ˍ₋arg1[1], (^)(ˍ₋arg1[2], 2))
                     nothing
                 end
         end
--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -1,15 +1,15 @@
 function (ˍ₋out, u)
-    #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:350 =#
-    #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:351 =#
-    #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:352 =#
+    #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:373 =#
+    #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:374 =#
+    #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:375 =#
     begin
-        #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:520 =#
-        #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:399 =# @inbounds begin
-                #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:395 =#
-                ˍ₋out[1] = (+)((*)(-1, (getindex)(u, 3)), (getindex)(u, 1))
-                ˍ₋out[2] = (+)((^)((getindex)(u, 1), 2), (*)(-1, (getindex)(u, 2)))
+        #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:537 =#
+        #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:422 =# @inbounds begin
+                #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:418 =#
+                ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
                 ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                #= /home/runner/.julia/packages/SymbolicUtils/H684H/src/code.jl:397 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/ssQsQ/src/code.jl:420 =#
                 nothing
             end
     end
