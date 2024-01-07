ab7e80504ce5ae026bb1591049230c8a3ff52eba
Jimmy Envall <jimmyenvall@gmail.com>
2024-01-07 13:23:55 +0100

    Add test comments

--- test/forwarddiff_symbolic_dual_ops.jl
+++ test/forwarddiff_symbolic_dual_ops.jl
@@ -9,6 +9,9 @@ SF = SymbolicUtils.SpecialFunctions
 
 @variables x
 
+# Test functions from Symbolics #
+#-------------------------------#
+
 for f ∈ SymbolicUtils.basic_monadic
     fun = eval(:(ξ ->($f)(ξ)))
 
@@ -96,3 +99,20 @@ for f ∈ (acsc, asech, NaNMath.log2, NaNMath.log10)
 
     @test isequal(fd, substitute(sym, Dict(x => 1.0)))
 end
+
+# Additionally test these definitions from ForwardDiff #
+#------------------------------------------------------#
+
+# https://github.com/JuliaDiff/ForwardDiff.jl/blob/d3002093beb88ff0b98ed178377961dfd55c1247/src/dual.jl#L599
+# and
+# https://github.com/JuliaDiff/ForwardDiff.jl/blob/d3002093beb88ff0b98ed178377961dfd55c1247/src/dual.jl#L683
+for f ∈ (hypot, muladd)
+    fun = eval(:(ξ ->($f)(ξ, 2.0, 3.0)))
+
+    fd = ForwardDiff.derivative(fun, 5.0)
+    sym = Symbolics.Differential(x)(fun(x)) |> expand_derivatives
+
+    @test isequal(fd, substitute(sym, Dict(x => 5.0)))
+end
+
+# fma is not defined for Symbolics.Num
