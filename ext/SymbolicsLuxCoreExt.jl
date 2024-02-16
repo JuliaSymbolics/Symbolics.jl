356e4c903f20c6fbe5ff0e2ae917e74edc103ded
Sebastian Micluța-Câmpeanu <sebastian.mc95@proton.me>
2024-02-16 04:16:00 +0200

    use `LuxCore.outputsize`

--- ext/SymbolicsLuxCoreExt.jl
+++ ext/SymbolicsLuxCoreExt.jl
@@ -3,14 +3,14 @@ module SymbolicsLuxCoreExt
 using LuxCore, Symbolics
 
 @register_array_symbolic LuxCore.partial_apply(
-    model::LuxCore.AbstractExplicitContainerLayer, x::AbstractArray, ps::NamedTuple, st::NamedTuple) begin
-    size = ((model[end].out_dims),)
+    model::LuxCore.AbstractExplicitContainerLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}, st::NamedTuple) begin
+    size = LuxCore.outputsize(model[end])
     eltype = Real
 end
 
 @register_array_symbolic LuxCore.partial_apply(
-    model::LuxCore.AbstractExplicitLayer, x::AbstractArray, ps::NamedTuple, st::NamedTuple) begin
-    size = ((model.out_dims),)
+    model::LuxCore.AbstractExplicitLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}, st::NamedTuple) begin
+    size = LuxCore.outputsize(model)
     eltype = Real
 end
 
