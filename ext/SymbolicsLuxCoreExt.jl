26ddc9c04d813a8ebf794fe447ccb39a354d66ba
Sebastian Micluța-Câmpeanu <sebastian.mc95@proton.me>
2024-02-25 03:04:56 +0200

    update api

--- ext/SymbolicsLuxCoreExt.jl
+++ ext/SymbolicsLuxCoreExt.jl
@@ -2,14 +2,14 @@ module SymbolicsLuxCoreExt
 
 using LuxCore, Symbolics
 
-@register_array_symbolic LuxCore.partial_apply(
-    model::LuxCore.AbstractExplicitContainerLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}, st::NamedTuple) begin
+@register_array_symbolic LuxCore.stateless_apply(
+    model::LuxCore.AbstractExplicitContainerLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}) begin
     size = LuxCore.outputsize(model[end])
     eltype = Real
 end
 
-@register_array_symbolic LuxCore.partial_apply(
-    model::LuxCore.AbstractExplicitLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}, st::NamedTuple) begin
+@register_array_symbolic LuxCore.stateless_apply(
+    model::LuxCore.AbstractExplicitLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}) begin
     size = LuxCore.outputsize(model)
     eltype = Real
 end
--- test/extensions/lux.jl
+++ test/extensions/lux.jl
@@ -5,18 +5,17 @@ using ComponentArrays
     model = Dense(5, 6)
     rng = Random.default_rng()
     x = randn(rng, Float32, 5)
-    ps, st = LuxCore.setup(rng, model)
+    ps, _ = LuxCore.setup(rng, model)
 
     ca = ComponentArray(ps)
     Symbolics.@variables sym_ca[1:length(ca)] = ca
     Symbolics.@variables sym_ps::typeof(ps) = ps
-    Symbolics.@variables sym_st::typeof(st) = st
     Symbolics.@variables sym_x[1:5] = Float32[1,2,3,4,5]
 
-    out_ref = LuxCore.partial_apply(model, x, ps, st)
+    out_ref = LuxCore.stateless_apply(model, x, ps)
     @test out_ref isa Vector{Float32}
 
-    out = LuxCore.partial_apply(model, sym_x, ps, st)
+    out = LuxCore.stateless_apply(model, sym_x, ps)
     # if the symbolic function regstration wouldn't work, we'd get the
     # vector propagated through the neural network and get a Vector{Num}
     @test out isa Symbolics.Arr
@@ -25,40 +24,22 @@ using ComponentArrays
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x),)))
     @test out_sub == out_ref
 
-    out = LuxCore.partial_apply(model, sym_x, sym_ps, st)
+    out = LuxCore.stateless_apply(model, sym_x, sym_ps)
     @test out isa Symbolics.Arr
     @test length(out) == 6
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ps=>ps),)))
+    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ps => ps),)))
     @test out_sub == out_ref
 
-    out = LuxCore.partial_apply(model, sym_x, ps, sym_st)
-    @test out isa Symbolics.Arr
-    @test length(out) == 6
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_st => st),)))
-    @test out_sub == out_ref
-
-    out = LuxCore.partial_apply(model, sym_x, sym_ps, sym_st)
-    @test out isa Symbolics.Arr
-    @test length(out) == 6
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ps => ps, sym_st => st),)))
-    @test out_sub == out_ref
-
-    out = LuxCore.partial_apply(model, sym_x, ca, st)
+    out = LuxCore.stateless_apply(model, sym_x, ca)
     @test out isa Symbolics.Arr
     @test length(out) == 6
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x,),)))
     @test out_sub == out_ref
 
-    out = LuxCore.partial_apply(model, sym_x, sym_ca, st)
+    out = LuxCore.stateless_apply(model, sym_x, sym_ca)
     @test out isa Symbolics.Arr
     @test length(out) == 6
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ca => ca),)))
-    @test out_sub == out_ref
-
-    out = LuxCore.partial_apply(model, sym_x, sym_ca, sym_st)
-    @test out isa Symbolics.Arr
-    @test length(out) == 6
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ca => ca, sym_st => st),)))
     @test out_sub == out_ref
 end
 
@@ -66,18 +47,17 @@ end
     model = Chain(Dense(5, 6), Dense(6, 2), Dense(2, 3))
     rng = Random.default_rng()
     x = randn(rng, Float32, 5)
-    ps, st = LuxCore.setup(rng, model)
+    ps, _ = LuxCore.setup(rng, model)
 
     ca = ComponentArray(ps)
     Symbolics.@variables sym_ca[1:length(ca)] = ca
     Symbolics.@variables sym_ps::typeof(ps) = ps
-    Symbolics.@variables sym_st::typeof(st) = st
     Symbolics.@variables sym_x[1:5] = Float32[1, 2, 3, 4, 5]
 
-    out_ref = LuxCore.partial_apply(model, x, ps, st)
+    out_ref = LuxCore.stateless_apply(model, x, ps)
     @test out_ref isa Vector{Float32}
 
-    out = LuxCore.partial_apply(model, sym_x, ps, st)
+    out = LuxCore.stateless_apply(model, sym_x, ps)
     # if the symbolic function regstration wouldn't work, we'd get the
     # vector propagated through the neural network and get a Vector{Num}
     @test out isa Symbolics.Arr
@@ -86,39 +66,21 @@ end
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x),)))
     @test out_sub == out_ref
 
-    out = LuxCore.partial_apply(model, sym_x, sym_ps, st)
+    out = LuxCore.stateless_apply(model, sym_x, sym_ps)
     @test out isa Symbolics.Arr
     @test length(out) == 3
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ps => ps),)))
     @test out_sub == out_ref
 
-    out = LuxCore.partial_apply(model, sym_x, ps, sym_st)
-    @test out isa Symbolics.Arr
-    @test length(out) == 3
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_st => st),)))
-    @test out_sub == out_ref
-
-    out = LuxCore.partial_apply(model, sym_x, sym_ps, sym_st)
-    @test out isa Symbolics.Arr
-    @test length(out) == 3
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ps => ps, sym_st => st),)))
-    @test out_sub == out_ref
-
-    out = LuxCore.partial_apply(model, sym_x, ca, st)
+    out = LuxCore.stateless_apply(model, sym_x, ca)
     @test out isa Symbolics.Arr
     @test length(out) == 3
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x,),)))
     @test out_sub == out_ref
 
-    out = LuxCore.partial_apply(model, sym_x, sym_ca, st)
+    out = LuxCore.stateless_apply(model, sym_x, sym_ca)
     @test out isa Symbolics.Arr
     @test length(out) == 3
     out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ca => ca),)))
-    @test out_sub == out_ref
-
-    out = LuxCore.partial_apply(model, sym_x, sym_ca, sym_st)
-    @test out isa Symbolics.Arr
-    @test length(out) == 3
-    out_sub = Symbolics.value.(Symbolics.substitute.(Symbolics.scalarize(out), (Dict(sym_x => x, sym_ca => ca, sym_st => st),)))
     @test out_sub == out_ref
 end
