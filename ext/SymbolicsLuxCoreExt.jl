a6f88bc69f5e0a08b6867f194f1eed13e2245126
Sebastian Micluța-Câmpeanu <sebastian.mc95@proton.me>
2024-02-26 14:03:02 +0200

    remove `AbstractExplicitContainerLayer` specialization
    we shoud just use the `outputsize` API

--- ext/SymbolicsLuxCoreExt.jl
+++ ext/SymbolicsLuxCoreExt.jl
@@ -3,12 +3,6 @@ module SymbolicsLuxCoreExt
 using LuxCore, Symbolics
 
 @register_array_symbolic LuxCore.stateless_apply(
-    model::LuxCore.AbstractExplicitContainerLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}) begin
-    size = LuxCore.outputsize(model[end])
-    eltype = Real
-end
-
-@register_array_symbolic LuxCore.stateless_apply(
     model::LuxCore.AbstractExplicitLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}) begin
     size = LuxCore.outputsize(model)
     eltype = Real
