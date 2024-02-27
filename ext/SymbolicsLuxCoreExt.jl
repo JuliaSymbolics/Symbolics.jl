d3ebacd06eead51a0f86387b0edd2f8b428634f9
Sebastian Micluța-Câmpeanu <sebastian.mc95@proton.me>
2024-02-27 13:58:57 +0200

    use the more general outputsize

--- ext/SymbolicsLuxCoreExt.jl
+++ ext/SymbolicsLuxCoreExt.jl
@@ -4,7 +4,7 @@ using LuxCore, Symbolics
 
 @register_array_symbolic LuxCore.stateless_apply(
     model::LuxCore.AbstractExplicitLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}) begin
-    size = LuxCore.outputsize(model)
+    size = LuxCore.outputsize(model, x, LuxCore._default_rng())
     eltype = Real
 end
 
