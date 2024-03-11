542832065469514628454dc26011d0bf7ba67392
Documenter.jl <documenter@juliadocs.github.io>
2024-03-11 06:05:20 +0000

    build based on 97c7b13

---
+++ v5.23.0/manual/function.jl
@@ -0,0 +1,16 @@
+function (ˍ₋out, u)
+    #= /home/runner/.julia/packages/SymbolicUtils/c0xQb/src/code.jl:373 =#
+    #= /home/runner/.julia/packages/SymbolicUtils/c0xQb/src/code.jl:374 =#
+    #= /home/runner/.julia/packages/SymbolicUtils/c0xQb/src/code.jl:375 =#
+    begin
+        #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:537 =#
+        #= /home/runner/.julia/packages/SymbolicUtils/c0xQb/src/code.jl:422 =# @inbounds begin
+                #= /home/runner/.julia/packages/SymbolicUtils/c0xQb/src/code.jl:418 =#
+                ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                #= /home/runner/.julia/packages/SymbolicUtils/c0xQb/src/code.jl:420 =#
+                nothing
+            end
+    end
+end
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v5.23",
   "v5.22",
   "v5.21",
   "v5.3",
@@ -35,5 +36,5 @@ var DOC_VERSIONS = [
   "v0.1",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v5.22.1";
+var DOCUMENTER_NEWEST = "v5.23.0";
 var DOCUMENTER_STABLE = "stable";
