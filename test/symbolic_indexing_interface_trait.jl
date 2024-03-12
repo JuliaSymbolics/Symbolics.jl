2be328e5188f880ca788285c58a1afdd8884180d
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2024-03-12 11:15:59 +0530

    feat: move fast_substitute to Symbolics, implement SII.symbolic_evaluate

--- test/symbolic_indexing_interface_trait.jl
+++ test/symbolic_indexing_interface_trait.jl
@@ -10,3 +10,11 @@ using SymbolicIndexingInterface
 @variables y[1:3]
 @test symbolic_type(y) == ArraySymbolic()
 @test all(symbolic_type.(collect(y)) .== (ScalarSymbolic(),))
+
+@variables x y z
+subs = Dict(x => 0.1, y => 2z)
+subs2 = merge(subs, Dict(z => 2x+3))
+
+@test symbolic_evaluate(x, subs) == 0.1
+@test isequal(symbolic_evaluate(y, subs), 2z)
+@test symbolic_evaluate(y, subs2) == 6.4
