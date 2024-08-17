5107a94e500ecb262827e641fac712904abd8a50
Sasha Demin <demin@lix.polytechnique.fr>
2024-08-17 17:26:54 +0200

    turn off formatting for some s

--- src/solver/attract.jl
+++ src/solver/attract.jl
@@ -110,14 +110,11 @@ function attract_logs(lhs, var)
     contains_var(arg) = n_occurrences(arg, var) > 0
 
     r_addlogs = Vector{Any}()
-    push!(r_addlogs,
-        @acrule log(~x::(contains_var)) + log(~y::(contains_var)) => slog(~x * ~y))
-    push!(r_addlogs,
-        @acrule ~z * log(~x::(contains_var)) + log(~y::(contains_var)) => slog((~x)^(~z) *
-                                                                               ~y))
-    push!(r_addlogs,
-        @acrule ~z * log(~x::(contains_var)) + ~h * log(~y::(contains_var)) => slog((~x)^(~z) *
-                                                                                    (~y)^(~h)))
+    #! format: off
+    push!(r_addlogs, @acrule log(~x::(contains_var)) + log(~y::(contains_var)) => slog(~x * ~y))
+    push!(r_addlogs, @acrule ~z*log(~x::(contains_var)) + log(~y::(contains_var)) => slog((~x)^(~z) * ~y))
+    push!(r_addlogs, @acrule ~z*log(~x::(contains_var)) + ~h*log(~y::(contains_var)) => slog((~x)^(~z) * (~y)^(~h)))
+    #! format: on
 
     lhs = expand(simplify(
         lhs, rewriter = SymbolicUtils.Postwalk(SymbolicUtils.Chain(r_addlogs))))
@@ -148,20 +145,12 @@ function attract_exponential(lhs, var)
     contains_var(arg) = n_occurrences(arg, var) > 0
 
     r_addexpon = Vector{Any}()
-    push!(r_addexpon,
-        @acrule (~b)^(~f::(contains_var)) + (~d)^(~g::(contains_var)) => ~f *
-                                                                         term(slog, ~b) -
-                                                                         ~g *
-                                                                         term(slog, ~d) +
-                                                                         term(
-            log, term(complex, -1)))
-    push!(r_addexpon,
-        @acrule (~a) * (~b)^(~f::(contains_var)) + (~d)^(~g::(contains_var)) => ~f * term(
-            slog, ~b) - ~g * term(slog, ~d) + term(slog, -~a))
-    push!(r_addexpon,
-        @acrule (~a) * (~b)^(~f::(contains_var)) + (~c) * (~d)^(~g::(contains_var)) => ~f *
-                                                                                       term(
-            slog, ~b) - ~g * term(slog, ~d) + term(slog, -(~a) // (~c)))
+
+    #! format: off
+    push!(r_addexpon, @acrule (~b)^(~f::(contains_var)) + (~d)^(~g::(contains_var)) => ~f*term(slog, ~b) - ~g*term(slog, ~d) + term(log, term(complex, -1)))
+    push!(r_addexpon, @acrule (~a)*(~b)^(~f::(contains_var)) + (~d)^(~g::(contains_var)) => ~f*term(slog, ~b) - ~g*term(slog, ~d) + term(slog, -~a))
+    push!(r_addexpon, @acrule (~a)*(~b)^(~f::(contains_var)) + (~c)*(~d)^(~g::(contains_var)) => ~f*term(slog, ~b) - ~g*term(slog, ~d) + term(slog, -(~a)//(~c)))
+    #! format: on
 
     lhs = expand(simplify(
         lhs, rewriter = SymbolicUtils.Postwalk(SymbolicUtils.Chain(r_addexpon))))
