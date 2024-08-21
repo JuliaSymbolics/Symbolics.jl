49a384580b71e7b23461a1201860092daf86b827
Documenter.jl <documenter@juliadocs.github.io>
2024-08-21 22:35:13 +0000

    build based on 2182fbd

---
+++ previews/PR1226/function.jl
@@ -0,0 +1,11 @@
+function (ˍ₋out, ˍ₋arg1)
+    begin
+        begin
+            #= /home/runner/.julia/packages/SymbolicUtils/ovxY7/src/code.jl:434 =# @inbounds begin
+                    ˍ₋out[1] = (+)(ˍ₋arg1[2], (^)(ˍ₋arg1[1], 2))
+                    ˍ₋out[2] = (+)(ˍ₋arg1[1], (^)(ˍ₋arg1[2], 2))
+                    nothing
+                end
+        end
+    end
+end
