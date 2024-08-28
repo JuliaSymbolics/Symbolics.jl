60b8fc6e1557e8e92d109bdf8230eabaa0cb946d
Daniel VandenHeuvel <95613936+DanielVandH@users.noreply.github.com>
2024-08-28 13:13:10 +0100

    Add missing at test

--- test/utils.jl
+++ test/utils.jl
@@ -23,9 +23,9 @@ end
 
 @testset "symbolic_to_float" begin
     @variables x
-    symbolic_to_float((1//2 * x)/x) isa Float64
-    symbolic_to_float((1/2 * x)/x) isa Float64
-    symbolic_to_float((1//2)*√(279//4)) isa Float64
-    symbolic_to_float((big(1)//2)*√(279//4)) isa BigFloat
-    symbolic_to_float((-1//2)*√(279//4)) isa Float64
+    @test symbolic_to_float((1//2 * x)/x) isa Rational{Int}
+    @test symbolic_to_float((1/2 * x)/x) isa Float64
+    @test symbolic_to_float((1//2)*√(279//4)) isa Float64
+    @test symbolic_to_float((big(1)//2)*√(279//4)) isa BigFloat
+    @test symbolic_to_float((-1//2)*√(279//4)) isa Float64
 end
