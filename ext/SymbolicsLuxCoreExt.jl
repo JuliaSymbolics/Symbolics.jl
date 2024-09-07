e78bc131552d71885a8bad67fdd82dc59e6fb6e3
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-09-07 12:51:00 -0400

    Update SymbolicsLuxCoreExt.jl

--- ext/SymbolicsLuxCoreExt.jl
+++ ext/SymbolicsLuxCoreExt.jl
@@ -4,7 +4,7 @@ using LuxCore, Symbolics
 
 @register_array_symbolic LuxCore.stateless_apply(
     model::LuxCore.AbstractLuxLayer, x::AbstractArray, ps::Union{NamedTuple, <:AbstractVector}) begin
-    size = LuxCore.outputsize(model, x, LuxCore._default_rng())
+    size = LuxCore.outputsize(model, x, LuxCore.Lux.Random())
     eltype = Real
 end
 
