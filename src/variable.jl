2eadc048a349c707c6011feeb730b9047676546e
Bowen S. Zhu <bowenzhu@mit.edu>
2024-09-27 14:19:38 -0400

    Remove `Sym` `depwarn`

--- src/variable.jl
+++ src/variable.jl
@@ -659,9 +659,3 @@ function (::Type{Variable{T}})(s, i...) where {T}
 end
 
 (::Type{Variable})(s, i...) = Variable{Real}(s, i...)
-
-function (::Type{Sym{T}})(s, x, i...) where {T}
-    Base.depwarn("Sym{T}(name, x, idx...) is deprecated, use variable(name, x, idx...; T=T)", :Variable, force=true)
-    variable(s, x, i...; T=T)
-end
-(::Type{Sym})(s, x, i...) = Sym{Real}(s, x, i...)
