e8316e81e4f91fc3754f1ba9e3f7066869a751ff
Bowen S. Zhu <bowenzhu@mit.edu>
2024-09-28 19:07:40 -0400

    Fix `Term` `arguments` `eltype` in test
    [skip ci]

--- test/struct.jl
+++ test/struct.jl
@@ -1,5 +1,6 @@
 using Test, Symbolics
-using Symbolics: symstruct, juliatype, symbolic_getproperty, symbolic_setproperty!, symbolic_constructor
+using Symbolics: symstruct, juliatype, symbolic_getproperty, symbolic_setproperty!,
+                 symbolic_constructor, BasicSymbolic
 
 struct Jörgen
     a::Int
@@ -11,15 +12,15 @@ S = symstruct(Jörgen)
 xa = Symbolics.unwrap(symbolic_getproperty(x, :a))
 @test Symbolics.symtype(xa) == Int
 @test Symbolics.operation(xa) == Symbolics.typed_getfield
-@test isequal(Symbolics.arguments(xa), [Symbolics.unwrap(x), Val{:a}()])
+@test isequal(Symbolics.arguments(xa), BasicSymbolic[Symbolics.unwrap(x), Val{:a}()])
 xa = Symbolics.unwrap(symbolic_setproperty!(x, :a, 10))
 @test Symbolics.operation(xa) == setfield!
-@test isequal(Symbolics.arguments(xa), [Symbolics.unwrap(x), Meta.quot(:a), 10])
+@test isequal(Symbolics.arguments(xa), BasicSymbolic[Symbolics.unwrap(x), Meta.quot(:a), 10])
 @test Symbolics.symtype(xa) == Int
 
 xb = Symbolics.unwrap(symbolic_setproperty!(x, :b, 10))
 @test Symbolics.operation(xb) == setfield!
-@test isequal(Symbolics.arguments(xb), [Symbolics.unwrap(x), Meta.quot(:b), 10])
+@test isequal(Symbolics.arguments(xb), BasicSymbolic[Symbolics.unwrap(x), Meta.quot(:b), 10])
 @test Symbolics.symtype(xb) == Float64
 
 s = Symbolics.symbolic_constructor(S, 1, 1.0)
