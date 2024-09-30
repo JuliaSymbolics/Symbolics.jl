b42e9f90e03e23ec2e32e9e447b6c43a2eb07b8b
Bowen S. Zhu <bowenzhu@mit.edu>
2024-09-30 16:01:58 -0400

    Fix a test due to `Term` arguments eltype change
    [skip ci]

--- test/macro.jl
+++ test/macro.jl
@@ -1,6 +1,6 @@
 using Symbolics
 import Symbolics: CallWithMetadata, getsource, getdefaultval, wrap, unwrap, getname
-import SymbolicUtils: _Term, symtype, FnType, BasicSymbolic, promote_symtype
+import SymbolicUtils: _Term, symtype, FnType, BasicSymbolic, promote_symtype, Symbolic
 using LinearAlgebra
 using Test
 
@@ -75,7 +75,7 @@ _args = [[a 2a; 4a 6a; 3a 5a], [4a, 6a]]
 hh = ccwa(_args...)
 @test size(hh) == (3, 2, 10)
 @test eltype(hh) == Real
-@test isequal(arguments(unwrap(hh)), unwrap.(_args))
+@test isequal(arguments(unwrap(hh)), convert(Vector{SymbolicUtils.Symbolic}, map(x -> unwrap.(x), _args)))
 
 @test all(t->getsource(t)[1] === :variables, many_vars)
 @test getdefaultval(t) == 0
