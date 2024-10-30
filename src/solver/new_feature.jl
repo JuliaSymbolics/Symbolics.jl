639cea498a112d4b5e7a6bf4cf9016f8e59edfac
n0rbed <eng.yassin.elbedwihy@gmail.com>
2024-10-30 22:47:49 +0300

    initial draft

--- src/solver/new_feature.jl
+++ src/solver/new_feature.jl
@@ -1,2 +1,17 @@
-function solve_eq_for_multivar(eq, vars)
+function solve_eq_for_multivar(eq, s)
+    args = arguments(unwrap(eq))
+    term_tm = 1
+    @info "" args
+    for arg in args
+        oper = operation(arg)
+        if oper == (/) 
+            term_tm *= wrap(arguments(arg)[2])
+            @info "" term_tm
+        end
+    end
+    args = [arg*term_tm for arg in args]
+    eq = expand(Symbolics.term((+), unwrap.(args)...))
+    coeffs, constant = polynomial_coeffs(eq, [s])
+    eqs = wrap.(collect(values(coeffs)))
+
 end
