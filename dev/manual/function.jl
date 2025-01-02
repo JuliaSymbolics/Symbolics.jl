4f1b20eeab6bdb675f09e89bac9cfebb4f21f998
Documenter.jl <documenter@juliadocs.github.io>
2025-01-02 13:47:53 +0000

    build based on 324c786

--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -1,16 +1,21 @@
 function (ˍ₋out, u)
-    #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:385 =#
-    #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:386 =#
-    #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:387 =#
-    begin
-        #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:542 =#
-        #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:434 =# @inbounds begin
-                #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:430 =#
-                ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
-                ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
-                ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:432 =#
-                nothing
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:342 =# @inbounds begin
+            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:342 =#
+            begin
+                #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:385 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:386 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:387 =#
+                begin
+                    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:558 =#
+                    #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:434 =# @inbounds begin
+                            #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:430 =#
+                            ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                            ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                            ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                            #= /home/runner/.julia/packages/SymbolicUtils/jf8aQ/src/code.jl:432 =#
+                            nothing
+                        end
+                end
             end
-    end
+        end
 end
