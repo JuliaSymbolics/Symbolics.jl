017b1a82f2f681b11c2dcc968688782749687245
Documenter.jl <documenter@juliadocs.github.io>
2025-01-16 14:16:59 +0000

    build based on b8dc023

---
+++ previews/PR1409/manual/function.jl
@@ -0,0 +1,21 @@
+function (ˍ₋out, u)
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:342 =# @inbounds begin
+            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:342 =#
+            begin
+                #= /home/runner/.julia/packages/SymbolicUtils/ibvgE/src/code.jl:388 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/ibvgE/src/code.jl:389 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/ibvgE/src/code.jl:390 =#
+                begin
+                    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:558 =#
+                    #= /home/runner/.julia/packages/SymbolicUtils/ibvgE/src/code.jl:437 =# @inbounds begin
+                            #= /home/runner/.julia/packages/SymbolicUtils/ibvgE/src/code.jl:433 =#
+                            ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                            ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                            ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                            #= /home/runner/.julia/packages/SymbolicUtils/ibvgE/src/code.jl:435 =#
+                            nothing
+                        end
+                end
+            end
+        end
+end
