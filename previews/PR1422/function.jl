60affd07aadf2d3ce9b5ae3efb2a32808f44dd35
Documenter.jl <documenter@juliadocs.github.io>
2025-01-29 11:31:01 +0000

    build based on f2733cd

---
+++ previews/PR1422/function.jl
@@ -0,0 +1,15 @@
+function (ˍ₋out, ˍ₋arg1)
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:347 =# @inbounds begin
+            begin
+                begin
+                    begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:437 =# @inbounds begin
+                                ˍ₋out[1] = (+)(ˍ₋arg1[2], (^)(ˍ₋arg1[1], 2))
+                                ˍ₋out[2] = (+)(ˍ₋arg1[1], (^)(ˍ₋arg1[2], 2))
+                                nothing
+                            end
+                    end
+                end
+            end
+        end
+end
