e2024dba9fc9abdad692915f66074a89c89d4392
Documenter.jl <documenter@juliadocs.github.io>
2025-01-31 11:49:05 +0000

    build based on d7ee676

--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -2,17 +2,17 @@ function (ˍ₋out, u)
     #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =# @inbounds begin
             #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =#
             begin
-                #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:388 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:389 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:390 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/6fncq/src/code.jl:389 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/6fncq/src/code.jl:390 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/6fncq/src/code.jl:391 =#
                 begin
                     #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:564 =#
-                    #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:437 =# @inbounds begin
-                            #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:433 =#
+                    #= /home/runner/.julia/packages/SymbolicUtils/6fncq/src/code.jl:438 =# @inbounds begin
+                            #= /home/runner/.julia/packages/SymbolicUtils/6fncq/src/code.jl:434 =#
                             ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
                             ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
                             ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                            #= /home/runner/.julia/packages/SymbolicUtils/2cE5G/src/code.jl:435 =#
+                            #= /home/runner/.julia/packages/SymbolicUtils/6fncq/src/code.jl:436 =#
                             nothing
                         end
                 end
