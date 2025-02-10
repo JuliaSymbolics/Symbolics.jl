772ec454d756406d69f344ba3433ca00f5c4eec5
Documenter.jl <documenter@juliadocs.github.io>
2025-02-10 17:01:49 +0000

    build based on 5a0cd56

---
+++ previews/PR1436/manual/function.jl
@@ -0,0 +1,21 @@
+function (ˍ₋out, u)
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =# @inbounds begin
+            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =#
+            begin
+                #= /home/runner/.julia/packages/SymbolicUtils/Su6xO/src/code.jl:389 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/Su6xO/src/code.jl:390 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/Su6xO/src/code.jl:391 =#
+                begin
+                    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:564 =#
+                    #= /home/runner/.julia/packages/SymbolicUtils/Su6xO/src/code.jl:438 =# @inbounds begin
+                            #= /home/runner/.julia/packages/SymbolicUtils/Su6xO/src/code.jl:434 =#
+                            ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                            ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                            ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                            #= /home/runner/.julia/packages/SymbolicUtils/Su6xO/src/code.jl:436 =#
+                            nothing
+                        end
+                end
+            end
+        end
+end
