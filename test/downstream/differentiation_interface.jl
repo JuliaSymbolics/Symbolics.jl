1a85b1c97b0d2a50633c008c343dbc2b3cc6d717
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-03-14 18:29:29 +0530

    Update test/downstream/differentiation_interface.jl
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>

--- test/downstream/differentiation_interface.jl
+++ test/downstream/differentiation_interface.jl
@@ -2,12 +2,6 @@ using DifferentiationInterface, DifferentiationInterfaceTest
 using Symbolics: Symbolics
 using Test
 
-LOGGING = get(ENV, "CI", "false") == "false"
-
-for backend in [AutoSymbolics(), AutoSparse(AutoSymbolics())]
-    @test check_available(backend)
-    @test check_inplace(backend)
-end
 
 test_differentiation(
     AutoSymbolics(), default_scenarios(; include_constantified=true); logging=LOGGING
