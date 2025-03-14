c4f25f53b578824ce3ac0ed62f04a556ecbe5384
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-03-14 18:29:38 +0530

    Update test/downstream/differentiation_interface.jl
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>

--- test/downstream/differentiation_interface.jl
+++ test/downstream/differentiation_interface.jl
@@ -3,20 +3,4 @@ using Symbolics: Symbolics
 using Test
 
 
-test_differentiation(
-    AutoSymbolics(), default_scenarios(; include_constantified=true); logging=LOGGING
-);
-
-test_differentiation(
-    AutoSymbolics(),
-    default_scenarios(; include_normal=false, include_cachified=true);
-    excluded=[:jacobian],  # TODO: figure out why this fails
-    logging=LOGGING,
-);
-
-test_differentiation(
-    AutoSparse(AutoSymbolics()),
-    sparse_scenarios(; band_sizes=0:-1);
-    sparsity=true,
-    logging=LOGGING,
-);
+test_differentiation(AutoSymbolics())
