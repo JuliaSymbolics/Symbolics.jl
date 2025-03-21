e23eb033d096034a1d28ee056ab470600d6f9603
Documenter.jl <documenter@juliadocs.github.io>
2025-03-21 19:32:33 +0000

    build based on 539a26f

--- dev/function.jl
+++ dev/function.jl
@@ -1,14 +1,12 @@
 function (ˍ₋out, ˍ₋arg1)
-    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =# @inbounds begin
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:358 =# @inbounds begin
             begin
                 begin
-                    begin
-                        #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:444 =# @inbounds begin
-                                ˍ₋out[1] = (+)(ˍ₋arg1[2], (^)(ˍ₋arg1[1], 2))
-                                ˍ₋out[2] = (+)(ˍ₋arg1[1], (^)(ˍ₋arg1[2], 2))
-                                nothing
-                            end
-                    end
+                    #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:457 =# @inbounds begin
+                            ˍ₋out[1] = (+)(ˍ₋arg1[2], (^)(ˍ₋arg1[1], 2))
+                            ˍ₋out[2] = (+)(ˍ₋arg1[1], (^)(ˍ₋arg1[2], 2))
+                            ˍ₋out
+                        end
                 end
             end
         end
--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -1,21 +1,18 @@
 function (ˍ₋out, u)
-    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =# @inbounds begin
-            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:348 =#
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:358 =# @inbounds begin
+            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:358 =#
             begin
-                #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:395 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:396 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:397 =#
-                begin
-                    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:564 =#
-                    #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:444 =# @inbounds begin
-                            #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:440 =#
-                            ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
-                            ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
-                            ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                            #= /home/runner/.julia/packages/SymbolicUtils/htTbU/src/code.jl:442 =#
-                            nothing
-                        end
-                end
+                #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:402 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:403 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:404 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:457 =# @inbounds begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:453 =#
+                        ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                        ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                        ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                        #= /home/runner/.julia/packages/SymbolicUtils/a0c2F/src/code.jl:455 =#
+                        ˍ₋out
+                    end
             end
         end
 end
