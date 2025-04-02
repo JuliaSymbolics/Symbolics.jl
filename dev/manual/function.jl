9d0f3fae5bb73f2105e91100483507dc5dbf7dfc
Documenter.jl <documenter@juliadocs.github.io>
2025-04-02 08:54:24 +0000

    build based on 5e10d24

--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -2,15 +2,15 @@ function (ˍ₋out, u)
     #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:368 =# @inbounds begin
             #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:368 =#
             begin
-                #= /home/runner/.julia/packages/SymbolicUtils/qsTQJ/src/code.jl:402 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/qsTQJ/src/code.jl:403 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/qsTQJ/src/code.jl:404 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/qsTQJ/src/code.jl:457 =# @inbounds begin
-                        #= /home/runner/.julia/packages/SymbolicUtils/qsTQJ/src/code.jl:453 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/MGDe2/src/code.jl:409 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/MGDe2/src/code.jl:410 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/MGDe2/src/code.jl:411 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/MGDe2/src/code.jl:464 =# @inbounds begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/MGDe2/src/code.jl:460 =#
                         ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
                         ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
                         ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                        #= /home/runner/.julia/packages/SymbolicUtils/qsTQJ/src/code.jl:455 =#
+                        #= /home/runner/.julia/packages/SymbolicUtils/MGDe2/src/code.jl:462 =#
                         ˍ₋out
                     end
             end
