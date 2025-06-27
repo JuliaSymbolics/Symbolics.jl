d6daa7b6e5ab9b965adec20631cfad90e0434052
LordOfFrogs <mx.tori.dell@gmail.com>
2025-06-27 11:59:43 -0400

    added display functionality for LinearODE

--- src/diffeqs/diffeqs.jl
+++ src/diffeqs/diffeqs.jl
@@ -8,10 +8,16 @@ struct LinearODE
     t::SymbolicUtils.Symbolic # independent variable
     p::AbstractArray # coefficient functions of t ordered in increasing order (p₁, p₂, ...)
     q # right hand side function of t, without any x
+    Dt::Differential
+    order::Int
 
     LinearODE(x::Num, t::Num, p, q) = new(value(x), value(t), p, q)
 end
 
+get_expression(eq::LinearODE) = (eq.Dt^eq.order)(eq.x) + sum([(eq.p[n])*(eq.Dt^n)(eq.x) for n = 1:length(eq.p)]) ~ eq.q
+
+Base.print(io::IO, eq::LinearODE) = print(io, "(D$(eq.t)^$(eq.order))$(eq.x) + " * join(["($(eq.p[length(eq.p)-n]))(D$(eq.t)^$(length(eq.p)-n))$(eq.x)" for n = 0:(eq.order-2)], " + ") * " ~ $(eq.q)")
+Base.show(io::IO, eq::LinearODE) = print(io, eq)
 
 is_homogeneous(eq::LinearODE) = isempty(Symbolics.get_variables(eq.q))
 
