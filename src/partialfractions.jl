2cb343cd47447ae797e73df2cdf7063204d97369
LordOfFrogs <mx.tori.dell@gmail.com>
2025-07-21 22:28:16 -0400

    Mostly working. Switched to cover-up method and solving a system of linear equations.

--- src/partialfractions.jl
+++ src/partialfractions.jl
@@ -1,73 +1,98 @@
-# https://dl.acm.org/doi/pdf/10.1145/800204.806314
+# used to represent linear or irreducible quadratic factors
+struct Factor
+    expr
+    root
+    multiplicity
+    x
+end
+
+function Factor(expr, multiplicity, x)
+    fac_rule = @rule x + ~r => -~r
+    return Factor(expr, fac_rule(expr), multiplicity, x)
+end
+
+function Base.isequal(a::Factor, b::Factor)
+    return isequal(a.expr, b.expr) && a.multiplicity == b.multiplicity
+end
+
+# https://math.mit.edu/~hrm/18.031/pf-coverup.pdf
+"""
+    partial_frac_decomposition(expr, x)
+
+Performs partial fraction decomposition for expressions with linear, reapeated, or irreducible quadratic factors in the denominator. Can't handle irrational roots or non-one leading coefficients
+
+!!! note that irreducible quadratic and repeated linear factors require the `Groebner` package to solve a system of equations
+"""
 function partial_frac_decomposition(expr, x)
-    frac_rule = @rule ~a/~b => (~a, ~b)
-    A, B = frac_rule(expr)
+    A, B = numerator(expr), denominator(expr)
     
-    X = []
-    Z = []
-    b = gcd(coeff_vector(B, x)...)
-    Y = x .- symbolic_solve(B, x, dropmultiplicity=false)
-    k = length(Y)
+    facs = factorize(B, x)
+    v = simplify(B / prod((f -> f.expr^f.multiplicity).(facs)))
     
-    if k == 1
+    if length(facs) == 1 && only(facs).multiplicity == 1 && degree(A) <= 1
         return expr
     end
 
-    # idk what this is for
-    # for i = 1:k-1
-    #     if degree(B[i]) == 0
-    #         push!(X, A)
-    #         push!(Z, b)
-    #         for j = 1:k-1
-    #             push!(X, 0)
-    #             push!(Z, 1)
-    #         end
-    #         return X, Y, Z
-    #     end
-    # end
-    
-    E = zeros(degree(B), degree(B))
-    F = coeff_vector(A, x)
+    result = 0
 
-    for i = 1:k
-        E[:, i] = coeff_vector(simplify(B/Y[i]), x, degree(B) - 1)
+    c_idx = 0
+    if length(facs) == 1
+        fac = only(facs)
+        if fac.root === nothing
+            for i = 1:fac.multiplicity
+                result += (variable(:C, c_idx+=1)*x + variable(:C, c_idx+=1))/(fac.expr^i)
+            end
+        else
+            result += sum(variables(:C, (c_idx+1):(c_idx+=fac.multiplicity)) ./ fac.expr.^(1:fac.multiplicity))
+        end
+    else
+        for fac in facs
+            if fac.root === nothing
+                for i = 1:fac.multiplicity
+                    result += (variable(:C, c_idx+=1)*x + variable(:C, c_idx+=1))/(fac.expr^i)
+                end
+                continue
+            end
+
+            other_facs = filter(f -> !isequal(f, fac), facs)
+            
+            numerator = rationalize(unwrap(substitute(A / prod((f -> f.expr^f.multiplicity).(other_facs)), Dict(x => fac.root))))
+            result += numerator / fac.expr^fac.multiplicity
+
+            if fac.multiplicity > 1
+                result += sum(variables(:C, (c_idx+1):(c_idx+=fac.multiplicity-1)) ./ fac.expr.^(1:fac.multiplicity-1))
+            end
+        end
     end
-    @show E
-    Gbar = rationalize.(inv(E), tol=1e-6)*F
-    G0 = Gbar[1]
-    G = Gbar[2:end] / G0
 
-    w = b * G0
-
-    j = 1
-    for i = 1:k
-        m = degree(Y[i])
+    if isequal(get_variables(result), [x])
+        return expand(result/v)
+    end
         
-        if m == 0
-            push!(X, 0)
-            push!(Z, 1)
-            continue
-        end
+    lhs::Vector{Rational} = coeff_vector(numerator(expr), x)
+    rhs = coeff_vector(numerator(simplify(result)), x)
 
-        Ai = G[j:j+m] .* x.^[0:m]
-        h = gcd(w, coeff_vector(Ai, x)...)
-        vi = w / h
-        Ai /= h
-
-        if vi < 0
-            vi *= -1
-            Ai *= -1
-        end
-
-        push!(X, Ai)
-        push!(Z, vi)
-
-        j += m
+    if length(lhs) > length(rhs)
+        rhs = [rhs; zeros(length(lhs)-length(rhs))]
+    elseif length(rhs) > length(lhs)
+        lhs = [lhs; zeros(length(rhs)-length(lhs))]
     end
 
-    return reverse(X) ./ (Y .* reverse(Z))
+    eqs = []
+    for i = 1:length(lhs)
+        push!(eqs, lhs[i] ~ rhs[i])
+    end
+    
+    solution = symbolic_solve(eqs, Symbolics.variables(:C, 1:c_idx))[1]
+
+    if !(solution isa Dict)
+        solution = Dict(variable(:C, 1) => solution)
+    end
+    
+    return expand(substitute(result, solution)/v)
 end
 
+# increasing from 0 to degree n
 function coeff_vector(poly, x, n)
     coeff_dict = polynomial_coeffs(poly, [x])[1]
     vec = []
@@ -82,6 +107,40 @@ function coeff_vector(poly, x, n)
     return vec
 end
 
+# increasing from 0 to degree of poly
 function coeff_vector(poly, x)
     return coeff_vector(poly, x, degree(poly))
+end
+
+function count_multiplicities(facs)
+    counts = Dict()
+    for fac in facs
+        if haskey(counts, fac)
+            counts[fac] += 1
+        else
+            counts[fac] = 1
+        end
+    end
+
+    return counts
+end
+
+# for partial fractions, into linear and irreducible quadratic factors
+function factorize(expr, x)::Set{Factor}
+    roots = symbolic_solve(expr, x, dropmultiplicity=false)
+
+    counts = count_multiplicities(roots)
+    facs = Set()
+
+    for root in keys(counts)
+        if !isequal(abs(imag(root)), 0)
+            fac_expr = real(expand(real((x - root)*(x - conj(root)))))
+            push!(facs, Factor(fac_expr, counts[root], x))
+            continue
+        end
+
+        push!(facs, Factor(x - root, root, counts[root], x))
+    end
+
+    return facs
 end
