6bf95bbdb1f966ed32e26802c1b28823a8493f54
LordOfFrogs <mx.tori.dell@gmail.com>
2025-08-25 09:55:21 -0400

    pull out transform rules into global scope

--- src/diffeqs/laplace.jl
+++ src/diffeqs/laplace.jl
@@ -1,5 +1,46 @@
 import DomainSets.ClosedInterval
 
+# from https://tutorial.math.lamar.edu/Classes/DE/Laplace_Table.aspx
+transform_rules(f, t, F, s) = Symbolics.Chain([
+    @rule 1 => 1/s
+    @rule exp(t) => 1/(s - 1)
+    @rule exp(~a * t) => 1/(-~a + s)
+    @rule t => 1/s^2
+    @rule t^~n => factorial(~n)/s^(~n + 1)
+    @rule sqrt(t) => term(sqrt, pi)/(2 * s^(3/2))
+    @rule sin(t) => 1/(1 + s^2)
+    @rule sin(~a * t) => ~a/((~a)^2 + s^2)
+    @rule cos(t) => s/(1 + s^2)
+    @rule cos(~a * t) => s/((~a)^2 + s^2)
+    @rule t*sin(t) => 1/(1 + s^2)^2
+    @rule t*sin(~a * t) => 2*~a*s / ((~a)^2 + s^2)^2
+    @rule t*cos(t) => (s^2 - 1) / (1 + s^2)^2
+    @rule t*cos(~a * t) => (-(~a)^2 + s^2) / ((~a)^2 + s^2)^2
+    @rule sin(t) - t*cos(t) => 2 / (1 + s^2)^2
+    @rule sin(~a*t) - ~a*t*cos(~a*t) => 2*(~a)^3 / ((~a)^2 + s^2)^2
+    @rule sin(t) + t*cos(t) => 2s^2 / (1 + s^2)^2
+    @rule sin(~a*t) + ~a*t*cos(~a*t) => 2*~a*s^2 / ((~a)^2 + s^2)^2
+    @rule cos(~a*t) - ~a*t*sin(~a*t) => s*((~a)^2 + s^2) / ((~a)^2 + s^2)^2
+    @rule cos(~a*t) + ~a*t*sin(~a*t) => s*(s^2 + 3*(~a)^2) / ((~a)^2 + s^2)^2
+    @rule sin(~b + ~a*t) => (s*sin(~b) + ~a*cos(~b)) / ((~a)^2 + s^2)
+    @rule cos(~b + ~a*t) => (s*cos(~b) - ~a*sin(~b)) / ((~a)^2 + s^2)
+    @rule sinh(~a * t) => ~a/(-(~a)^2 + s^2)
+    @rule cosh(~a * t) => s/(-(~a)^2 + s^2)
+    @rule exp(~a*t) * sin(~b * t) => ~b / ((~b)^2 + (-~a+s)^2)
+    @rule exp(~a*t) * cos(~b * t) => (-~a+s) / ((~b)^2 + (-~a+s)^2)
+    @rule exp(~a*t) * sinh(~b * t) => ~b / (-(~b)^2 + (-~a+s)^2)
+    @rule exp(~a*t) * cosh(~b * t) => (-~a+s) / (-(~b)^2 + (-~a+s)^2)
+    @rule t^~n * exp(~a * t) => factorial(~n) / (-~a + s)^(~n + 1)
+    @rule t*exp(~a * t) => 1 / (-~a + s)^(2)
+    @rule t^~n * exp(t) => factorial(~n) / (s)^(~n + 1)
+    @rule t*exp(t) => 1 / (s)^(2)
+    @rule exp(~c*t) * ~g => laplace(~g, f, t, F, s - ~c) # s-shift rule
+    @rule t*f(t) => -Ds(F(s)) # s-derivative rule
+    @rule t^(~n)*f(t) => (-1)^(~n) * (Ds^~n)(F(s)) # s-derivative rule
+    @rule f(~a + t) => exp(~a*s)*F(s) # t-shift rule
+    @rule f(t) => F(s)
+])
+
 """
     laplace(expr, f, t, F, s)
 
@@ -13,48 +54,8 @@ function laplace(expr, f, t, F, s)
     expr = expand(expr)
     Dt = Differential(t)
     Ds = Differential(s)
-    # from https://tutorial.math.lamar.edu/Classes/DE/Laplace_Table.aspx
-    transform_rules = Symbolics.Chain([
-        @rule 1 => 1/s
-        @rule exp(t) => 1/(s - 1)
-        @rule exp(~a * t) => 1/(-~a + s)
-        @rule t => 1/s^2
-        @rule t^~n => factorial(~n)/s^(~n + 1)
-        @rule sqrt(t) => term(sqrt, pi)/(2 * s^(3/2))
-        @rule sin(t) => 1/(1 + s^2)
-        @rule sin(~a * t) => ~a/((~a)^2 + s^2)
-        @rule cos(t) => s/(1 + s^2)
-        @rule cos(~a * t) => s/((~a)^2 + s^2)
-        @rule t*sin(t) => 1/(1 + s^2)^2
-        @rule t*sin(~a * t) => 2*~a*s / ((~a)^2 + s^2)^2
-        @rule t*cos(t) => (s^2 - 1) / (1 + s^2)^2
-        @rule t*cos(~a * t) => (-(~a)^2 + s^2) / ((~a)^2 + s^2)^2
-        @rule sin(t) - t*cos(t) => 2 / (1 + s^2)^2
-        @rule sin(~a*t) - ~a*t*cos(~a*t) => 2*(~a)^3 / ((~a)^2 + s^2)^2
-        @rule sin(t) + t*cos(t) => 2s^2 / (1 + s^2)^2
-        @rule sin(~a*t) + ~a*t*cos(~a*t) => 2*~a*s^2 / ((~a)^2 + s^2)^2
-        @rule cos(~a*t) - ~a*t*sin(~a*t) => s*((~a)^2 + s^2) / ((~a)^2 + s^2)^2
-        @rule cos(~a*t) + ~a*t*sin(~a*t) => s*(s^2 + 3*(~a)^2) / ((~a)^2 + s^2)^2
-        @rule sin(~b + ~a*t) => (s*sin(~b) + ~a*cos(~b)) / ((~a)^2 + s^2)
-        @rule cos(~b + ~a*t) => (s*cos(~b) - ~a*sin(~b)) / ((~a)^2 + s^2)
-        @rule sinh(~a * t) => ~a/(-(~a)^2 + s^2)
-        @rule cosh(~a * t) => s/(-(~a)^2 + s^2)
-        @rule exp(~a*t) * sin(~b * t) => ~b / ((~b)^2 + (-~a+s)^2)
-        @rule exp(~a*t) * cos(~b * t) => (-~a+s) / ((~b)^2 + (-~a+s)^2)
-        @rule exp(~a*t) * sinh(~b * t) => ~b / (-(~b)^2 + (-~a+s)^2)
-        @rule exp(~a*t) * cosh(~b * t) => (-~a+s) / (-(~b)^2 + (-~a+s)^2)
-        @rule t^~n * exp(~a * t) => factorial(~n) / (-~a + s)^(~n + 1)
-        @rule t*exp(~a * t) => 1 / (-~a + s)^(2)
-        @rule t^~n * exp(t) => factorial(~n) / (s)^(~n + 1)
-        @rule t*exp(t) => 1 / (s)^(2)
-        @rule exp(~c*t) * ~g => laplace(~g, f, t, F, s - ~c) # s-shift rule
-        @rule t*f(t) => -Ds(F(s)) # s-derivative rule
-        @rule t^(~n)*f(t) => (-1)^(~n) * (Ds^~n)(F(s)) # s-derivative rule
-        @rule f(~a + t) => exp(~a*s)*F(s) # t-shift rule
-        @rule f(t) => F(s)
-    ])
 
-    transformed = transform_rules(expr)
+    transformed = transform_rules(f, t, F, s)(expr)
     if !isequal(transformed, expr)
         return transformed
     end
@@ -93,6 +94,36 @@ function laplace(expr::Equation, f, t, F, s)
     return laplace(expr.lhs, f, t, F, s) ~ laplace(expr.rhs, f, t, F, s)
 end
 
+# postprocess_root prevents automatic evaluation of sqrt to its floating point value
+function _sqrt(x)
+    return postprocess_root(term(sqrt, x))
+end
+
+# F and f aren't used here, but are here for future-proofing
+inverse_transform_rules(F, s, f, t) = Symbolics.Chain([
+    @rule 1/s => 1
+    @rule 1/(~a + s) => exp(-~a * t)
+    @rule 1/s^(~n) => t^(~n-1) / factorial(~n-1)
+    @rule 1/(2 * s^(3/2)) => sqrt(t)/term(term(sqrt, pi))
+    @rule 1/(~a + s^2) => sin(_sqrt(~a) * t)/_sqrt(~a)
+    @rule s/(~a + s^2) => cos(_sqrt(~a) * t)
+    @rule s / (~a + s^2)^2 => t*sin(_sqrt(~a) * t)/(2*_sqrt(~a))
+    @rule (-~a + s^2) / (~a + s^2)^2 => t*cos(_sqrt(~a) * t)
+    @rule 1 / (~a + s^2)^2 => (sin(_sqrt(~a)*t) - _sqrt(~a)*t*cos(_sqrt(~a)*t))/ (2*_sqrt(~a)^3)
+    @rule s^2 / (~a + s^2)^2 => (sin(_sqrt(~a)*t) + _sqrt(~a)*t*cos(_sqrt(~a)*t)) / (2*_sqrt(~a))
+    @rule s*(~a + s^2) / (~a + s^2)^2 => cos(_sqrt(~a)*t) - _sqrt(~a)*t*sin(_sqrt(~a)*t)
+    @rule s*(3*~a + s^2) / (~a + s^2)^2 => cos(_sqrt(~a)*t) + _sqrt(~a)*t*sin(_sqrt(~a)*t)
+    @rule (s*sin(~b) + ~a*cos(~b)) / (~a + s^2) => sin(~b + _sqrt(~a)*t)
+    @rule (s*cos(~b) - ~a*sin(~b)) / ((~a)^2 + s^2) => cos(~b + ~a*t)
+    @rule 1/(s^2 - (~b)^2) => sinh(~b * t)/~b
+    @rule s/(s^2 - (~b)^2) => cosh(~b * t)
+    @rule 1 / ((~c+s)^2 + (~b)^2) => exp(-~c*t) * sin(~b * t) / ~b
+    @rule (~c+s) / ((~c+s)^2 + (~b)^2) => exp(-~c*t) * cos(~b * t)
+    @rule 1 / ((~c+s)^2 - (~b)^2) => exp(-~c*t) * sinh(~b * t) / ~b
+    @rule (~c+s) / ((~c+s)^2 - (~b)^2) => exp(-~c*t) * cosh(~b * t)
+    @rule 1 / (~a + s)^(~n) => t^(~n-1) * exp(-~a * t) / factorial(~n-1)
+])
+
 """
     inverse_laplace(expr, F, s, f, t)
 
@@ -113,31 +144,7 @@ function inverse_laplace(expr, F, s, f, t)
         return inverse_laplace(partial_fractions, F, s, f, t)
     end
 
-    inverse_transform_rules = Symbolics.Chain([
-        @rule 1/s => 1
-        @rule 1/(~a + s) => exp(-~a * t)
-        @rule 1/s^(~n) => t^(~n-1) / factorial(~n-1)
-        @rule 1/(2 * s^(3/2)) => sqrt(t)/term(term(sqrt, pi))
-        @rule 1/(~a + s^2) => sin(postprocess_root(term(sqrt, ~a)) * t)/postprocess_root(term(sqrt, ~a))
-        @rule s/(~a + s^2) => cos(postprocess_root(term(sqrt, ~a)) * t)
-        @rule s / (~a + s^2)^2 => t*sin(postprocess_root(term(sqrt, ~a)) * t)/(2*postprocess_root(term(sqrt, ~a)))
-        @rule (-~a + s^2) / (~a + s^2)^2 => t*cos(postprocess_root(term(sqrt, ~a)) * t)
-        @rule 1 / (~a + s^2)^2 => (sin(postprocess_root(term(sqrt, ~a))*t) - postprocess_root(term(sqrt, ~a))*t*cos(postprocess_root(term(sqrt, ~a))*t))/ (2*postprocess_root(term(sqrt, ~a))^3)
-        @rule s^2 / (~a + s^2)^2 => (sin(postprocess_root(term(sqrt, ~a))*t) + postprocess_root(term(sqrt, ~a))*t*cos(postprocess_root(term(sqrt, ~a))*t)) / (2*postprocess_root(term(sqrt, ~a)))
-        @rule s*(~a + s^2) / (~a + s^2)^2 => cos(postprocess_root(term(sqrt, ~a))*t) - postprocess_root(term(sqrt, ~a))*t*sin(postprocess_root(term(sqrt, ~a))*t)
-        @rule s*(3*~a + s^2) / (~a + s^2)^2 => cos(postprocess_root(term(sqrt, ~a))*t) + postprocess_root(term(sqrt, ~a))*t*sin(postprocess_root(term(sqrt, ~a))*t)
-        @rule (s*sin(~b) + ~a*cos(~b)) / (~a + s^2) => sin(~b + postprocess_root(term(sqrt, ~a))*t)
-        @rule (s*cos(~b) - ~a*sin(~b)) / ((~a)^2 + s^2) => cos(~b + ~a*t)
-        @rule 1/(s^2 - (~b)^2) => sinh(~b * t)/~b
-        @rule s/(s^2 - (~b)^2) => cosh(~b * t)
-        @rule 1 / ((~c+s)^2 + (~b)^2) => exp(-~c*t) * sin(~b * t) / ~b
-        @rule (~c+s) / ((~c+s)^2 + (~b)^2) => exp(-~c*t) * cos(~b * t)
-        @rule 1 / ((~c+s)^2 - (~b)^2) => exp(-~c*t) * sinh(~b * t) / ~b
-        @rule (~c+s) / ((~c+s)^2 - (~b)^2) => exp(-~c*t) * cosh(~b * t)
-        @rule 1 / (~a + s)^(~n) => t^(~n-1) * exp(-~a * t) / factorial(~n-1)
-    ])
-
-    transformed = inverse_transform_rules(expr)
+    transformed = inverse_transform_rules(F, s, f, t)(expr)
     if !isequal(transformed, expr)
         return transformed
     end
