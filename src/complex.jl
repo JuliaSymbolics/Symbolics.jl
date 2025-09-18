34429bc9ab0d5fe18c0b0f50c9781069402f9a80
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-09-18 15:20:13 +0530

    fix: fix `unwrap` and `show` for `Complex{Num}`

--- src/complex.jl
+++ src/complex.jl
@@ -11,6 +11,32 @@ end
 
 function SymbolicUtils.unwrap(a::Complex{<:Num})
     re, img = unwrap(real(a)), unwrap(imag(a))
+    if SymbolicUtils.isconst(re) && SymbolicUtils.isconst(img)
+        return Const{VartypeT}(complex(unwrap_const(re), unwrap_const(img)))
+    end
+    if iscall(re) && operation(re) === real && iscall(img) && operation(img) === imag && isequal(arguments(re)[1], arguments(img)[1])
+        return arguments(re)[1]
+    end
     sT = promote_type(symtype(re), symtype(img))
-    return Term{vartype(re)}(complex, SymbolicUtils.ArgsT{vartype(re)}((re, img)); type = Complex{sT}, shape = SymbolicUtils.ShapeVecT())
+    return Term{VartypeT}(complex, SymbolicUtils.ArgsT{vartype(re)}((re, img)); type = Complex{sT}, shape = SymbolicUtils.ShapeVecT())
+end
+
+function Base.Complex{Num}(x::BasicSymbolic{VartypeT})
+    Complex{Num}(wrap(real(x)), wrap(imag(x)))
+end
+
+const IM = Sym{VartypeT}(:im; type = Number)
+
+function Base.show(io::IO, a::Complex{Num})
+    rr = unwrap(real(a))
+    ii = unwrap(imag(a))
+
+    if iscall(rr) && (operation(rr) === real) &&
+        iscall(ii) && (operation(ii) === imag) &&
+        isequal(arguments(rr)[1], arguments(ii)[1])
+
+        return print(io, arguments(rr)[1])
+    end
+
+    show(io, real(a) + IM * imag(a))
 end
