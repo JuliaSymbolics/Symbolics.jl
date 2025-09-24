e7048b45eb8d108aaf10dd789444d259fb8272c0
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-09-24 16:37:28 +0530

    refactor: update LuxExt

--- ext/SymbolicsLuxExt.jl
+++ ext/SymbolicsLuxExt.jl
@@ -7,13 +7,20 @@ using Lux.Random: AbstractRNG, default_rng
 using Symbolics.SymbolicUtils
 
 @static if isdefined(Lux.NilSizePropagation, :recursively_nillify)
-    function Lux.NilSizePropagation.recursively_nillify(x::SymbolicUtils.BasicSymbolic{<:Vector{<:Real}})
+    function Lux.NilSizePropagation.recursively_nillify(x::SymbolicUtils.BasicSymbolic)
+        @assert SymbolicUtils.symtype(x) <: Vector{<:Real}
         Lux.NilSizePropagation.recursively_nillify(Symbolics.wrap(x))
     end
 end
 
-function LuxCore.outputsize(model::SymbolicUtils.BasicSymbolic{<:LuxCore.AbstractLuxLayer}, x::Symbolics.Arr, rng::AbstractRNG)
-    LuxCore.outputsize(Symbolics.getdefaultval(model), x, rng)
+function LuxCore.outputsize(model::SymbolicUtils.BasicSymbolic, x::Symbolics.Arr, rng::AbstractRNG)
+    @assert SymbolicUtils.symtype(model) <: LuxCore.AbstractLuxLayer
+    concrete_model = if SymbolicUtils.isconst(model)
+        SymbolicUtils.unwrap_const(model)
+    else
+        Symbolics.getdefaultval(model)
+    end
+    LuxCore.outputsize(concrete_model, x, rng)
 end
 
 @register_array_symbolic LuxCore.stateless_apply(
