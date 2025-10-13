00bfc5c7e887057abffa914f2ccad195e353694d
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-10-13 16:04:58 +0530

    test: update invalidations test to be 1.12 compatible

--- test/invalidations.jl
+++ test/invalidations.jl
@@ -1,9 +1,9 @@
-using SnoopCompile: @snoop_invalidations
+using SnoopCompile: @snoop_invalidations, invalidation_trees
 using Symbolics
 
 struct FakeType end
 
 @testset "`option_to_metadata_type`" begin
     invs = @snoop_invalidations Symbolics.option_to_metadata_type(::Val{:____!_internal_999}) = FakeType
-    @test isempty(invs)
+    @test isempty(invalidation_trees(invs))
 end
