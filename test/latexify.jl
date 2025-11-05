0292746a4dc054e58654112c9fe2afba165f62a8
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-11-05 11:57:30 +0530

    test: fix latexify tests

--- test/latexify.jl
+++ test/latexify.jl
@@ -45,10 +45,8 @@ Dy = Differential(y)
 @test_reference "latexify_refs/stable_mul_ordering1.txt" latexify(x * y)
 @test_reference "latexify_refs/stable_mul_ordering2.txt" latexify(y * x)
 
-@test_throws TypeError latexify(x ~ y + z)
-# @test_reference "latexify_refs/equation1.txt" latexify(x ~ y + z)
-@test_throws TypeError latexify(x ~ Dx(y + z))
-# @test_reference "latexify_refs/equation2.txt" latexify(x ~ Dx(y + z))
+@test_reference "latexify_refs/equation1.txt" latexify(x ~ y + z)
+@test_reference "latexify_refs/equation2.txt" latexify(x ~ Dx(y + z))
 @test_reference "latexify_refs/equation5.txt" latexify(AA^2 + AA + 1 + X₁)
 
 @test_reference "latexify_refs/equation_vec1.txt" latexify([
