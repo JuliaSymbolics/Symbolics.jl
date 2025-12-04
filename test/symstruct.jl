b828da25735fdac426f545eeaa133912227ec43d
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-12-04 18:34:25 +0530

    test: add tests for subtyping relations with symbolic struct registration

--- test/symstruct.jl
+++ test/symstruct.jl
@@ -100,3 +100,98 @@ end
     @test val == 1 + 2 + 3.0 + 5.0
 end
 
+abstract type AbstractRecord1 end
+
+@symstruct AbstractRecord1 begin
+    shape(:x) = [1:3]
+end
+
+struct ConcreteRecord1_1 <: AbstractRecord1
+    x::Vector{Int}
+end
+
+struct ConcreteRecord1_2 <: AbstractRecord1
+    x::Vector{Int}
+end
+
+@symstruct ConcreteRecord1_2 begin
+    shape(:x) = [1:2]
+end
+
+record_op1(x::ConcreteRecord1_1) = sum(x.x)
+record_op1(x::ConcreteRecord1_2) = prod(x.x)
+
+@register_symbolic record_op1(x::AbstractRecord1)
+
+record_arrop1(x::Vector{<:AbstractRecord1}) = sum(record_op1, x)
+
+@register_symbolic record_arrop1(x::Vector{AbstractRecord1})
+
+@testset "`@symstruct` of simple abstract type" begin
+    @test Symbolics.has_symwrapper(AbstractRecord1)
+    @test Symbolics.wrapper_type(AbstractRecord1) == SymStruct{<:AbstractRecord1}
+    @test Symbolics.wrapper_type(ConcreteRecord1_1) === SymStruct{ConcreteRecord1_1}
+
+    @variables r1::ConcreteRecord1_1 r2::ConcreteRecord1_2
+    @test SU.shape(r1.x) == [1:3]
+    @test SU.shape(r2.x) == [1:2]
+end
+
+@testset "Function registration with symbolic structs" begin
+    @variables r1::ConcreteRecord1_1 r2::ConcreteRecord1_2
+    ex = record_op1(r1)
+    @test operation(SU.unwrap(ex)) === record_op1
+
+    val = eval(quote
+        let r1 = ConcreteRecord1_1([2,2,3])
+            $(toexpr(ex))
+        end
+    end)
+    @test val == 7
+
+    ex = record_op1(r2)
+    @test operation(SU.unwrap(ex)) === record_op1
+
+    val = eval(quote
+        let r2 = ConcreteRecord1_2([2,2,3])
+            $(toexpr(ex))
+        end
+    end)
+    @test val == 12
+
+    @variables r3[1:2]::ConcreteRecord1_1 r4[1:2]::ConcreteRecord1_2
+
+    ex = record_arrop1(r3)
+    @test operation(SU.unwrap(ex)) === record_arrop1
+    val = eval(quote
+        let r3 = [ConcreteRecord1_1([2,3,4]), ConcreteRecord1_1([3,4,5])]
+            $(toexpr(ex))
+        end
+    end)
+    @test val == 21
+
+    ex = record_arrop1(r4)
+    @test operation(SU.unwrap(ex)) === record_arrop1
+    val = eval(quote
+        let r4 = [ConcreteRecord1_2([2,3,4]), ConcreteRecord1_2([3,4,5])]
+            $(toexpr(ex))
+        end
+    end)
+    @test val == 84
+
+    ex = record_arrop1([r3[1], r1])
+    @test operation(SU.unwrap(ex)) === record_arrop1
+    ex = record_arrop1([r4[1], r2])
+    @test operation(SU.unwrap(ex)) === record_arrop1
+end
+
+abstract type AbstractRecord2{T} end
+
+@symstruct AbstractRecord2
+
+@testset "Registering type without parameters works" begin
+    @test Symbolics.has_symwrapper(AbstractRecord2)
+    @test Symbolics.wrapper_type(AbstractRecord2) == SymStruct{<:AbstractRecord2}
+    @test Symbolics.has_symwrapper(AbstractRecord2{Int})
+    @test Symbolics.wrapper_type(AbstractRecord2{Int}) == SymStruct{<:AbstractRecord2{Int}}
+end
