26f15e3dd1df0e889c4be1fa83ac333b540e64fb
Documenter.jl <documenter@juliadocs.github.io>
2026-02-02 12:53:11 +0000

    build based on 64072e4

--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -2,15 +2,15 @@ function (ˍ₋out, u)
     #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:382 =# @inbounds begin
             #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:382 =#
             begin
-                #= /home/runner/.julia/packages/SymbolicUtils/kgV0N/src/code.jl:585 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/kgV0N/src/code.jl:586 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/kgV0N/src/code.jl:587 =#
-                #= /home/runner/.julia/packages/SymbolicUtils/kgV0N/src/code.jl:640 =# @inbounds begin
-                        #= /home/runner/.julia/packages/SymbolicUtils/kgV0N/src/code.jl:636 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/yIyQT/src/code.jl:584 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/yIyQT/src/code.jl:585 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/yIyQT/src/code.jl:586 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/yIyQT/src/code.jl:639 =# @inbounds begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/yIyQT/src/code.jl:635 =#
                         ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
                         ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
                         ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                        #= /home/runner/.julia/packages/SymbolicUtils/kgV0N/src/code.jl:638 =#
+                        #= /home/runner/.julia/packages/SymbolicUtils/yIyQT/src/code.jl:637 =#
                         ˍ₋out
                     end
             end
