5fb285e062ee2b2eed8ceb6534150808cc714833
Documenter.jl <documenter@juliadocs.github.io>
2026-04-07 10:25:18 +0000

    build based on 700b3d2

---
+++ previews/PR1832/manual/function.jl
@@ -0,0 +1,15 @@
+function (ˍ₋out, u)
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:383 =# @inbounds begin
+            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:383 =#
+            begin
+                #= /home/runner/.julia/packages/SymbolicUtils/GiJXZ/src/code.jl:1110 =# @inbounds begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/GiJXZ/src/code.jl:1106 =#
+                        ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                        ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                        ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                        #= /home/runner/.julia/packages/SymbolicUtils/GiJXZ/src/code.jl:1108 =#
+                        ˍ₋out
+                    end
+            end
+        end
+end
