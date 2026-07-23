d8b96653a5ee9a384d8b071cdda212bf6075cbe4
Tori <mx.tori.dell@gmail.com>
2026-07-23 13:09:47 -0400

    working differentiation for many expressions

--- test/derivativegraph.jl
+++ test/derivativegraph.jl
@@ -1,15 +1,17 @@
 using Symbolics
 using SymbolicUtils
 using Test
-using Symbolics: value, unwrap
+using Symbolics: value, unwrap, derivative
 
-@variables x y
+@variables x
+D = Differential(x)
 
-test_roots = [x, 2x, x^2, sin(cos(x))*cos(cos(x)), cos(sin(exp(2x)) + cos(exp(2x))), 2sin(x^4 - x) + 3cos(2x), 2x*exp(x) + 2*exp(x)]
+@test_broken isequal(derivative(x, x), expand_derivatives(D(x)))
+@test isequal(derivative(2x, x), expand_derivatives(D(2x)))
+@test isequal(derivative(x^2, x), expand_derivatives(D(x^2)))
+@test isequal(derivative(sin(cos(x))*cos(cos(x)), x), expand_derivatives(D(sin(cos(x))*cos(cos(x)))))
+@test isequal(expand(derivative(cos(sin(exp(2x)) + cos(exp(2x))), x)), expand(expand_derivatives(D(cos(sin(exp(2x)) + cos(exp(2x)))))))
 
-dgs = []
-
-for root in test_roots
-    dg = Symbolics.DerivativeGraph([unwrap(root)], [unwrap(x)], Int)
-    push!(dgs, dg)
-end
+# TODO: reevaluation of altered subgraphs
+@test_broken isequal(derivative(2sin(x^4 - x) + 3cos(2x), x), expand_derivatives(D(2sin(x^4 - x) + 3cos(2x))))
+@test_broken isequal(derivative(2x*exp(x) + 2*exp(x), x), expand_derivatives(D(2x*exp(x) + 2*exp(x))))
