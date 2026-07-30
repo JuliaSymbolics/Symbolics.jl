4f53fb114cecd62af48d2a336f6b41f4d4868add
Tori <mx.tori.dell@gmail.com>
2026-07-30 13:53:38 -0400

    fixed dom_mask calculation (all tests pass)

--- test/derivativegraph.jl
+++ test/derivativegraph.jl
@@ -11,7 +11,5 @@ D = Differential(x)
 @test isequal(dstar_derivative(x^2, x), expand_derivatives(D(x^2)))
 @test isequal(dstar_derivative(sin(cos(x))*cos(cos(x)), x), expand_derivatives(D(sin(cos(x))*cos(cos(x)))))
 @test isequal(expand(dstar_derivative(cos(sin(exp(2x)) + cos(exp(2x))), x)), expand(expand_derivatives(D(cos(sin(exp(2x)) + cos(exp(2x)))))))
-
-# TODO: reevaluation of altered subgraphs
-@test_broken isequal(dstar_derivative(2sin(x^4 - x) + 3cos(2x), x), expand_derivatives(D(2sin(x^4 - x) + 3cos(2x))))
-@test_broken isequal(dstar_derivative(2x*exp(x) + 2*exp(x), x), expand_derivatives(D(2x*exp(x) + 2*exp(x))))
+@test isequal(expand(dstar_derivative(2sin(x^4 - x) + 3cos(2x), x)), expand_derivatives(D(2sin(x^4 - x) + 3cos(2x))))
+@test isequal(dstar_derivative(2x*exp(x) + 2*exp(x), x), expand(expand_derivatives(D(2x*exp(x) + 2*exp(x)))))
