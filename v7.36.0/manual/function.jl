6c24e8c67993b48d2e9700e791827ef70e4b7b73
Documenter.jl <documenter@juliadocs.github.io>
2026-08-07 02:10:38 +0000

    build based on 30e97f6

---
+++ v7.36.0/manual/function.jl
@@ -0,0 +1,15 @@
+function (ˍ₋out, u)
+    #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:410 =# @inbounds begin
+            #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:410 =#
+            begin
+                #= /home/runner/.julia/packages/SymbolicUtils/Lyfmt/src/code.jl:1219 =# @inbounds begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/Lyfmt/src/code.jl:1215 =#
+                        ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
+                        ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
+                        ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
+                        #= /home/runner/.julia/packages/SymbolicUtils/Lyfmt/src/code.jl:1217 =#
+                        ˍ₋out
+                    end
+            end
+        end
+end
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v7.36",
   "v7.35",
   "v7.34",
   "v7.33",
@@ -137,5 +138,5 @@ var DOC_VERSIONS = [
   "v0.1",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v7.35.0";
+var DOCUMENTER_NEWEST = "v7.36.0";
 var DOCUMENTER_STABLE = "stable";
