2848dd2a639e4a177cc6682b6dd19000b2b55bec
Documenter.jl <documenter@juliadocs.github.io>
2026-08-09 03:42:28 +0000

    build based on f18fb78

--- dev/manual/function.jl
+++ dev/manual/function.jl
@@ -2,12 +2,12 @@ function (ˍ₋out, u)
     #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:410 =# @inbounds begin
             #= /home/runner/work/Symbolics.jl/Symbolics.jl/src/build_function.jl:410 =#
             begin
-                #= /home/runner/.julia/packages/SymbolicUtils/Lyfmt/src/code.jl:1219 =# @inbounds begin
-                        #= /home/runner/.julia/packages/SymbolicUtils/Lyfmt/src/code.jl:1215 =#
+                #= /home/runner/.julia/packages/SymbolicUtils/1Ldx8/src/code.jl:1219 =# @inbounds begin
+                        #= /home/runner/.julia/packages/SymbolicUtils/1Ldx8/src/code.jl:1215 =#
                         ˍ₋out[1] = (+)((getindex)(u, 1), (*)(-1, (getindex)(u, 3)))
                         ˍ₋out[2] = (+)((*)(-1, (getindex)(u, 2)), (^)((getindex)(u, 1), 2))
                         ˍ₋out[3] = (+)((getindex)(u, 2), (getindex)(u, 3))
-                        #= /home/runner/.julia/packages/SymbolicUtils/Lyfmt/src/code.jl:1217 =#
+                        #= /home/runner/.julia/packages/SymbolicUtils/1Ldx8/src/code.jl:1217 =#
                         ˍ₋out
                     end
             end
